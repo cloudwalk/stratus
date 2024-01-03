686decad3c72b333d80f73c57dd6707068cbf471
Arthur M Meskelis <arthur.meskelis@cloudwalk.io>
2024-01-03 18:35:35 -0300

    using deserializer from block selection

--- src/eth/primitives/block_selection.rs
+++ src/eth/primitives/block_selection.rs
@@ -1,8 +1,7 @@
 use crate::eth::primitives::BlockNumber;
 use crate::eth::primitives::Hash;
-use serde::{Deserialize, Serialize};
 
-#[derive(Debug, Clone, PartialEq, Eq, Serialize, Deserialize)]
+#[derive(Debug, Clone, PartialEq, Eq, serde::Serialize)]
 pub enum BlockSelection {
     /// Retrieve the most recent block.
     Latest,
@@ -20,28 +19,28 @@ impl Default for BlockSelection {
     }
 }
 
-// impl<'de> serde::Deserialize<'de> for BlockSelection {
-//     fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
-//     where
-//         D: serde::Deserializer<'de>,
-//     {
-//         let value = String::deserialize(deserializer)?.to_lowercase();
-//         match value.as_str() {
-//             // parse special keywords
-//             "latest" => Ok(Self::Latest),
-//             // parse hash (64: H256 without 0x prefix; 66: H256 with 0x prefix)
-//             s if s.len() == 64 || s.len() == 66 => {
-//                 let hash: Hash = s.parse().map_err(serde::de::Error::custom)?;
-//                 Ok(Self::Hash(hash))
-//             }
-//             // parse number
-//             s => {
-//                 let number: BlockNumber = s.parse().map_err(serde::de::Error::custom)?;
-//                 Ok(Self::Number(number))
-//             }
-//         }
-//     }
-// }
+impl<'de> serde::Deserialize<'de> for BlockSelection {
+    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+    where
+        D: serde::Deserializer<'de>,
+    {
+        let value = String::deserialize(deserializer)?.to_lowercase();
+        match value.as_str() {
+            // parse special keywords
+            "latest" => Ok(Self::Latest),
+            // parse hash (64: H256 without 0x prefix; 66: H256 with 0x prefix)
+            s if s.len() == 64 || s.len() == 66 => {
+                let hash: Hash = s.parse().map_err(serde::de::Error::custom)?;
+                Ok(Self::Hash(hash))
+            }
+            // parse number
+            s => {
+                let number: BlockNumber = s.parse().map_err(serde::de::Error::custom)?;
+                Ok(Self::Number(number))
+            }
+        }
+    }
+}
 
 #[cfg(test)]
 mod tests {
