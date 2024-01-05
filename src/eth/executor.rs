f03afdb9c93463ec012a26d4508eea9d5f417b22
Renato Dinhani <101204870+dinhani-cw@users.noreply.github.com>
2024-01-05 11:43:53 -0300

    feat: newHeads and logs subscriptions (#36)

--- src/eth/executor.rs
+++ src/eth/executor.rs
@@ -7,6 +7,8 @@ use crate::eth::primitives::CallInput;
 use crate::eth::primitives::StoragerPointInTime;
 use crate::eth::primitives::TransactionExecution;
 use crate::eth::primitives::TransactionInput;
+use crate::eth::rpc::RpcBlockNotifier;
+use crate::eth::rpc::RpcLogNotifier;
 use crate::eth::storage::EthStorage;
 use crate::eth::EthError;
 
@@ -15,18 +17,36 @@ pub struct EthExecutor {
     evm: Mutex<Box<dyn Evm>>,
     miner: Mutex<BlockMiner>,
     eth_storage: Arc<dyn EthStorage>,
+
+    rpc_block_notifier: Option<RpcBlockNotifier>,
+    rpc_log_notifier: Option<RpcLogNotifier>,
 }
 
 impl EthExecutor {
+    /// Creates a new executor.
     pub fn new(evm: Box<impl Evm>, eth_storage: Arc<dyn EthStorage>) -> Self {
         Self {
             evm: Mutex::new(evm),
             miner: Mutex::new(BlockMiner::new(Arc::clone(&eth_storage))),
             eth_storage,
+            rpc_block_notifier: None,
+            rpc_log_notifier: None,
         }
     }
 
+    /// Sets the RPC block notifier.
+    pub fn set_rpc_block_notifier(&mut self, block_notifier: RpcBlockNotifier) {
+        self.rpc_block_notifier = Some(block_notifier);
+    }
+
+    /// Sets the RPC log notifier.
+    pub fn set_rpc_log_notifier(&mut self, log_notifier: RpcLogNotifier) {
+        self.rpc_log_notifier = Some(log_notifier);
+    }
+
     /// Execute a transaction, mutate the state and return function output.
+    ///
+    /// TODO: too much cloning that can be optimized here.
     pub fn transact(&self, transaction: TransactionInput) -> Result<TransactionExecution, EthError> {
         let signer = transaction.signer()?;
 
@@ -53,9 +73,25 @@ impl EthExecutor {
 
         // execute, mine and save
         let execution = evm_lock.execute(transaction.clone().try_into()?)?;
-        if execution.is_success() {
-            let block = miner_lock.mine_with_one_transaction(signer, transaction, execution.clone())?;
-            self.eth_storage.save_block(block)?;
+        let block = miner_lock.mine_with_one_transaction(signer, transaction, execution.clone())?;
+        self.eth_storage.save_block(block.clone())?;
+
+        // notify new blocks
+        if let Some(block_notifier) = &self.rpc_block_notifier {
+            if let Err(e) = block_notifier.send(block.clone()) {
+                tracing::error!(reason = ?e, "failed to send block notification");
+            };
+        }
+
+        // notify transaction logs
+        if let Some(log_notifier) = &self.rpc_log_notifier {
+            for trx in block.transactions {
+                for log in trx.logs {
+                    if let Err(e) = log_notifier.send(log) {
+                        tracing::error!(reason = ?e, "failed to send log notification");
+                    };
+                }
+            }
         }
 
         Ok(execution)
--- src/eth/rpc/mod.rs
+++ src/eth/rpc/mod.rs
@@ -4,6 +4,7 @@ mod rpc_context;
 mod rpc_middleware;
 mod rpc_parser;
 mod rpc_server;
+mod rpc_subscriptions;
 
 use rpc_context::RpcContext;
 use rpc_middleware::RpcMiddleware;
@@ -11,3 +12,6 @@ use rpc_parser::next_rpc_param;
 use rpc_parser::parse_rpc_rlp;
 pub use rpc_parser::rpc_internal_error;
 pub use rpc_server::serve_rpc;
+pub use rpc_subscriptions::RpcBlockNotifier;
+pub use rpc_subscriptions::RpcLogNotifier;
+pub use rpc_subscriptions::RpcSubscriptions;
