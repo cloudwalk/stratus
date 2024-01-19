ef441c6f7fda669e769bf6cd099ef90826771f9d
Arthur M Meskelis <arthur.meskelis@cloudwalk.io>
2024-01-19 14:00:05 -0300

    returning removed line

--- src/eth/primitives/block_selection.rs
+++ src/eth/primitives/block_selection.rs
@@ -28,6 +28,7 @@ impl<'de> serde::Deserialize<'de> for BlockSelection {
         match value.as_str() {
             // parse special keywords
             "latest" => Ok(Self::Latest),
+            "pending" => Ok(Self::Latest),
             // parse hash (64: H256 without 0x prefix; 66: H256 with 0x prefix)
             s if s.len() == 64 || s.len() == 66 => {
                 let hash: Hash = s.parse().map_err(serde::de::Error::custom)?;
