e0c53929da4ee55e2b7c00edf7348d8e53d29f6c
arthur-cw <arthur.moreira@cloudwalk.io>
2024-01-23 18:24:27 -0300

    lint

--- src/eth/storage/postgres/postgres.rs
+++ src/eth/storage/postgres/postgres.rs
@@ -9,7 +9,7 @@ use crate::eth::primitives::BlockSelection;
 use crate::eth::primitives::Hash;
 use crate::eth::primitives::LogFilter;
 use crate::eth::primitives::LogMined;
-use crate::eth::primitives::LogTopic;
+
 use crate::eth::primitives::Slot;
 use crate::eth::primitives::SlotIndex;
 use crate::eth::primitives::StoragePointInTime;
