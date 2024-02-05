1c5d400b499c6efb86fb7ff1d673930c8d514ebb
Daniel Freire <daniel.freire@cloudwalk.io>
2024-02-05 12:51:40 -0300

    Added comments explaining the reset queries

--- src/eth/storage/postgres/postgres.rs
+++ src/eth/storage/postgres/postgres.rs
@@ -681,13 +681,18 @@ impl EthStorage for Postgres {
             .execute(&self.connection_pool)
             .await?;
 
+        // Rollback the values of account.latest_balance, account.latest_nonce and
+        // account_slots.value.
+
         sqlx::query!(
             r#"
+            -- Get the block_numbers with the latest balance change for each address
             WITH latest_block_numbers AS (
                 SELECT address, MAX(block_number) as block_number
                 FROM historical_balances
                 GROUP BY address
             ),
+            -- Get the latest balance change for each address
             latest_balances AS (
                 SELECT accounts.address, historical_balances.balance
                 FROM accounts
@@ -696,6 +701,7 @@ impl EthStorage for Postgres {
                 LEFT JOIN historical_balances on accounts.address = historical_balances.address
                     AND latest_block_numbers.block_number = historical_balances.block_number
             )
+            -- Update the accounts with the balances in latest_balances
             UPDATE accounts
             SET latest_balance = latest_balances.balance
             FROM latest_balances
