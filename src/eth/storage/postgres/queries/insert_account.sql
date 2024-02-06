72f8b0c3c85853859764ad60db7c3e5f01772bdb
carneiro-cw <156914855+carneiro-cw@users.noreply.github.com>
2024-02-06 17:37:39 -0300

    feat: check conflicts on postgres (#159)
    * check for conclicts before insertion
    
    * Add comments, move slice
    
    ---------
    
    Co-authored-by: Arthur Franco Moreira <70960269+arthur-cw@users.noreply.github.com>

--- src/eth/storage/postgres/queries/insert_account.sql
+++ src/eth/storage/postgres/queries/insert_account.sql
@@ -1,5 +1,7 @@
 INSERT INTO accounts (address, latest_nonce, latest_balance, bytecode, creation_block)
-VALUES ($1, $2, $3, $4, $5) ON CONFLICT (address) DO
+VALUES ($1, $2, $3, $4, $5)
+ON CONFLICT (address) DO
 UPDATE
 SET latest_nonce = EXCLUDED.latest_nonce,
     latest_balance = EXCLUDED.latest_balance
+WHERE accounts.latest_nonce = $6 AND accounts.latest_balance = $7
