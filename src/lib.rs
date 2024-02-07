a4331949ca0aec4354a151b3e48618f5608d896e
Renato Dinhani <101204870+dinhani-cw@users.noreply.github.com>
2024-02-07 15:27:11 -0300

    common initialization (#166)

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,15 @@
+use clap::Parser;
+use config::Config;
+
 pub mod config;
 pub mod eth;
 pub mod ext;
 pub mod infra;
+
+/// Executes global services initialization.
+pub fn init_global_services() -> Config {
+    let config = Config::parse();
+    infra::init_tracing();
+    infra::init_metrics();
+    config
+}
