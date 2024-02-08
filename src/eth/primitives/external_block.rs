bee0f924fa90dc09986942a9ca6d784c9ff9e102
Renato Dinhani <renato.dinhani@cloudwalk.io>
2024-02-08 18:01:44 -0300

    feat: import_offline

--- src/eth/primitives/external_block.rs
+++ src/eth/primitives/external_block.rs
@@ -1,7 +1,30 @@
 use ethers_core::types::Block as EthersBlock;
 
+use super::BlockNumber;
 use crate::eth::primitives::ExternalTransaction;
+use crate::log_and_err;
 
 #[derive(Debug, Clone, derive_more:: Deref, serde::Deserialize, serde::Serialize)]
 #[serde(transparent)]
-pub struct ExternalBlock(#[deref] EthersBlock<ExternalTransaction>);
+pub struct ExternalBlock(#[deref] pub EthersBlock<ExternalTransaction>);
+
+impl ExternalBlock {
+    /// Returns the block number.
+    pub fn number(&self) -> BlockNumber {
+        self.0.number.expect("external block must have number").into()
+    }
+}
+
+// -----------------------------------------------------------------------------
+// Conversions: Other -> Self
+// -----------------------------------------------------------------------------
+impl TryFrom<serde_json::Value> for ExternalBlock {
+    type Error = anyhow::Error;
+
+    fn try_from(value: serde_json::Value) -> Result<Self, Self::Error> {
+        match serde_json::from_value(value.clone()) {
+            Ok(v) => Ok(v),
+            Err(e) => log_and_err!(reason = e, payload = value, "failed to convert payload value to ExternalBlock"),
+        }
+    }
+}
--- src/eth/primitives/external_transaction.rs
+++ src/eth/primitives/external_transaction.rs
@@ -2,4 +2,4 @@ use ethers_core::types::Transaction as EthersTransaction;
 
 #[derive(Debug, Clone, Default, derive_more:: Deref, serde::Deserialize, serde::Serialize)]
 #[serde(transparent)]
-pub struct ExternalTransaction(#[deref] EthersTransaction);
+pub struct ExternalTransaction(#[deref] pub EthersTransaction);
