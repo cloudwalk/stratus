4ab173930aede3b075bde010dd84d5456fed23eb
Arthur Franco Moreira <70960269+arthur-cw@users.noreply.github.com>
2024-02-09 13:04:13 -0300

    Bring #179 back to main (#188)
    * add intermediate step for in-between blocks execution
    
    * add metrified impl
    
    * fix deadlock on InMemoryStorageState write
    
    * use mut reference to InMemoryStorageState instead of RwWriteLockGuard
    
    * lint
    
    ---------
    
    Co-authored-by: Maycon Amaro <131882788+mayconamaroCW@users.noreply.github.com>

--- src/eth/storage/mod.rs
+++ src/eth/storage/mod.rs
@@ -3,6 +3,7 @@
 mod eth_storage;
 mod inmemory;
 mod metrified;
+mod overlay;
 mod postgres;
 mod storage_error;
 
@@ -10,4 +11,5 @@ pub use eth_storage::test_accounts;
 pub use eth_storage::EthStorage;
 pub use inmemory::InMemoryStorage;
 pub use metrified::MetrifiedStorage;
+pub use overlay::Overlay;
 pub use storage_error::EthStorageError;
