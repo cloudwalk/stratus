5cd86510c1a9a8390a83c286dde6d84c7aeced35
Daniel Freire <daniel.freire@cloudwalk.io>
2024-02-22 16:03:18 -0300

    doc

--- src/eth/storage/inmemory/mod.rs
+++ src/eth/storage/inmemory/mod.rs
@@ -5,6 +5,10 @@ mod inmemory_permanent;
 mod inmemory_state;
 mod inmemory_temporary;
 
+/// The inmemory storage is split into two structs. InMemoryStoragePermanent and
+/// InMemoryStorageTemporary to facilitate debugging when using the inmemory storage
+/// for both temp and perm contexts.
+
 pub use inmemory_history::InMemoryHistory;
 pub use inmemory_permanent::InMemoryStoragePermanent;
 pub use inmemory_temporary::InMemoryStorageTemporary;
