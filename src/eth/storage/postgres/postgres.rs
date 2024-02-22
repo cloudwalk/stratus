928b26a72a8a961f800934da61601bdb9e074827
Renato Dinhani <renato.dinhani@cloudwalk.io>
2024-02-22 12:35:50 -0300

    feat: remove enable_genesis method from trait and use existing functions to determine if genesis should be created

--- src/eth/storage/postgres/postgres.rs
+++ src/eth/storage/postgres/postgres.rs
@@ -34,6 +34,7 @@ use crate::eth::storage::postgres::types::PostgresTransaction;
 use crate::eth::storage::PermanentStorage;
 use crate::eth::storage::StorageError;
 use crate::infra::postgres::Postgres;
+use crate::log_and_err;
 
 #[async_trait]
 impl PermanentStorage for Postgres {
@@ -140,7 +141,7 @@ impl PermanentStorage for Postgres {
                 let block_number = i64::try_from(current)?;
 
                 let header_query = sqlx::query_file_as!(BlockHeader, "src/eth/storage/postgres/queries/select_block_header_by_number.sql", block_number,)
-                    .fetch_one(&self.connection_pool);
+                    .fetch_optional(&self.connection_pool);
 
                 let transactions_query = sqlx::query_file_as!(
                     PostgresTransaction,
@@ -162,7 +163,11 @@ impl PermanentStorage for Postgres {
                 // run queries concurrently, but not in parallel
                 // see https://docs.rs/tokio/latest/tokio/macro.join.html#runtime-characteristics
                 let res = tokio::join!(header_query, transactions_query, logs_query, topics_query);
-                let header = res.0?;
+                let header = match res.0 {
+                    Ok(Some(header)) => header,
+                    Ok(None) => return Ok(None),
+                    Err(e) => return log_and_err!(reason = e, "failed to query block by latest"),
+                };
                 let transactions = res.1?;
                 let logs = res.2?.into_iter();
                 let topics = res.3?.into_iter();
@@ -188,7 +193,7 @@ impl PermanentStorage for Postgres {
 
             BlockSelection::Hash(hash) => {
                 let header_query = sqlx::query_file_as!(BlockHeader, "src/eth/storage/postgres/queries/select_block_header_by_hash.sql", hash.as_ref(),)
-                    .fetch_one(&self.connection_pool);
+                    .fetch_optional(&self.connection_pool);
 
                 let transactions_query = sqlx::query_file_as!(
                     PostgresTransaction,
@@ -206,7 +211,11 @@ impl PermanentStorage for Postgres {
                 // run queries concurrently, but not in parallel
                 // see https://docs.rs/tokio/latest/tokio/macro.join.html#runtime-characteristics
                 let res = tokio::join!(header_query, transactions_query, logs_query, topics_query);
-                let header = res.0?;
+                let header = match res.0 {
+                    Ok(Some(header)) => header,
+                    Ok(None) => return Ok(None),
+                    Err(e) => return log_and_err!(reason = e, "failed to query block by hash"),
+                };
                 let transactions = res.1?;
                 let logs = res.2?.into_iter();
                 let topics = res.3?.into_iter();
@@ -234,7 +243,7 @@ impl PermanentStorage for Postgres {
                 let block_number = i64::try_from(*number)?;
 
                 let header_query = sqlx::query_file_as!(BlockHeader, "src/eth/storage/postgres/queries/select_block_header_by_number.sql", block_number,)
-                    .fetch_one(&self.connection_pool);
+                    .fetch_optional(&self.connection_pool);
 
                 let transactions_query = sqlx::query_file_as!(
                     PostgresTransaction,
@@ -256,7 +265,11 @@ impl PermanentStorage for Postgres {
                 // run queries concurrently, but not in parallel
                 // see https://docs.rs/tokio/latest/tokio/macro.join.html#runtime-characteristics
                 let res = tokio::join!(header_query, transactions_query, logs_query, topics_query);
-                let header = res.0?;
+                let header = match res.0 {
+                    Ok(Some(header)) => header,
+                    Ok(None) => return Ok(None),
+                    Err(e) => return log_and_err!(reason = e, "failed to query block by number"),
+                };
                 let transactions = res.1?;
                 let logs = res.2?.into_iter();
                 let topics = res.3?.into_iter();
@@ -283,7 +296,7 @@ impl PermanentStorage for Postgres {
                 let block_number = 0i64;
 
                 let header_query = sqlx::query_file_as!(BlockHeader, "src/eth/storage/postgres/queries/select_block_header_by_number.sql", block_number,)
-                    .fetch_one(&self.connection_pool);
+                    .fetch_optional(&self.connection_pool);
 
                 let transactions_query = sqlx::query_file_as!(
                     PostgresTransaction,
@@ -305,7 +318,11 @@ impl PermanentStorage for Postgres {
                 // run queries concurrently, but not in parallel
                 // see https://docs.rs/tokio/latest/tokio/macro.join.html#runtime-characteristics
                 let res = tokio::join!(header_query, transactions_query, logs_query, topics_query);
-                let header = res.0?;
+                let header = match res.0 {
+                    Ok(Some(header)) => header,
+                    Ok(None) => return Ok(None),
+                    Err(e) => return log_and_err!(reason = e, "failed to query block by earlist"),
+                };
                 let transactions = res.1?;
                 let logs = res.2?.into_iter();
                 let topics = res.3?.into_iter();
@@ -725,18 +742,6 @@ impl PermanentStorage for Postgres {
         sqlx::query_file!("src/eth/storage/postgres/queries/update_account_slots_reset_value.sql")
             .execute(&self.connection_pool)
             .await?;
-
-        Ok(())
-    }
-
-    async fn enable_genesis(&self, genesis: Block) -> anyhow::Result<()> {
-        let existing_genesis = sqlx::query_file!("src/eth/storage/postgres/queries/select_genesis.sql")
-            .fetch_optional(&self.connection_pool)
-            .await?;
-
-        if existing_genesis.is_none() {
-            self.save_block(genesis).await?;
-        }
 
         Ok(())
     }
