e9a14fc941efa82ab8c8916cef9417387b975e0b
Renato Dinhani <renato.dinhani@cloudwalk.io>
2024-02-28 13:19:42 -0300

    feat: improving block loading

--- src/bin/importer/importer-import.rs
+++ src/bin/importer/importer-import.rs
@@ -1,19 +1,35 @@
 mod _postgres;
 
+use std::cmp::min;
 use std::collections::HashMap;
 use std::sync::Arc;
 
 use _postgres::*;
+use anyhow::anyhow;
+use futures::try_join;
+use futures::StreamExt;
 use itertools::Itertools;
 use stratus::config::ImporterImportConfig;
 use stratus::eth::primitives::Account;
+use stratus::eth::primitives::BlockNumber;
+use stratus::eth::primitives::BlockSelection;
+use stratus::eth::storage::StratusStorage;
+use stratus::eth::EthExecutor;
+use stratus::ext::not;
 use stratus::infra::postgres::Postgres;
 use stratus::init_global_services;
+use stratus::log_and_err;
 use tokio::sync::mpsc;
 use tokio_util::sync::CancellationToken;
 
-/// Number of tasks in backlog: (BACKLOG_SIZE * BacklogTask)
+/// Number of blocks fetched in each query.
+const BLOCKS_BY_FETCH: usize = 50_000;
+
+/// Number of tasks in the backlog.
+///
+/// Each task contains 50_000 blocks and all receipts for them.
 const BACKLOG_SIZE: usize = 10;
+
 type BacklogTask = (Vec<BlockRow>, Vec<ReceiptRow>);
 
 #[tokio::main(flavor = "current_thread")]
@@ -25,19 +41,35 @@ async fn main() -> anyhow::Result<()> {
     let executor = config.init_executor(Arc::clone(&storage));
 
     // init shared data between importer and postgres loader
-    let (backlog_tx, mut backlog_rx) = mpsc::channel::<BacklogTask>(BACKLOG_SIZE);
+    let (backlog_tx, backlog_rx) = mpsc::channel::<BacklogTask>(BACKLOG_SIZE);
     let cancellation = CancellationToken::new();
 
     // import genesis accounts
-    let balances = pg_retrieve_downloaded_balances(&pg).await?;
+    let balances = pg_retrieve_external_balances(&pg).await?;
     let accounts = balances
         .into_iter()
         .map(|row| Account::new_with_balance(row.address, row.balance))
         .collect_vec();
     storage.save_accounts_to_perm(accounts).await?;
 
-    // load blocks and receipts in background
-    tokio::spawn(keep_loading_blocks(pg, cancellation.clone(), backlog_tx));
+    // execute parallel tasks (postgres loader and block importer)
+    tokio::spawn(execute_postgres_loader(pg, storage, cancellation.clone(), config.paralellism, backlog_tx));
+    execute_block_importer(executor, cancellation, backlog_rx).await?;
+
+    Ok(())
+}
+
+// -----------------------------------------------------------------------------
+// Importer
+// -----------------------------------------------------------------------------
+async fn execute_block_importer(
+    // services
+    executor: EthExecutor,
+    cancellation: CancellationToken,
+    // data
+    mut backlog_rx: mpsc::Receiver<BacklogTask>,
+) -> anyhow::Result<()> {
+    tracing::info!("block importer starting");
 
     // import blocks and transactions in foreground
     let reason = loop {
@@ -61,60 +93,92 @@ async fn main() -> anyhow::Result<()> {
             executor.import_offline(block.payload, &receipts_by_hash).await?;
         }
     };
-    tracing::info!(%reason, "importer finished");
 
+    tracing::info!(%reason, "block importer finished");
     Ok(())
 }
 
 // -----------------------------------------------------------------------------
 // Postgres block loader
 // -----------------------------------------------------------------------------
-async fn keep_loading_blocks(
+async fn execute_postgres_loader(
     // services
     pg: Arc<Postgres>,
+    storage: Arc<StratusStorage>,
     cancellation: CancellationToken,
     // data
+    paralellism: usize,
     backlog: mpsc::Sender<BacklogTask>,
 ) -> anyhow::Result<()> {
-    let mut tx = pg_init_blocks_cursor(&pg).await?;
+    tracing::info!("postgres loader starting");
 
-    let reason = loop {
-        if cancellation.is_cancelled() {
-            break "importer finished or failed";
-        }
-
-        // find blocks
-        tracing::info!("retrieving blocks to process");
-        let blocks = match pg_fetch_blocks(&mut tx).await {
-            Ok(blocks) =>
-                if blocks.is_empty() {
-                    cancellation.cancel();
-                    break "no more blocks to process";
-                } else {
-                    blocks
-                },
-            Err(_) => {
-                cancellation.cancel();
-                break "error loading blocks";
-            }
-        };
-
-        // find receipts
-        tracing::info!("retrieving receipts to process");
-        let block_start = blocks.first().unwrap().number;
-        let block_end = blocks.last().unwrap().number;
-        let Ok(receipts) = pg_retrieve_downloaded_receipts(&pg, block_start, block_end).await else {
+    // find block limits to load
+    let mut start = storage.read_current_block_number().await?;
+    if not(start.is_zero()) || storage.read_block(&BlockSelection::Number(BlockNumber::ZERO)).await?.is_some() {
+        start = start.next();
+    };
+    let end = match pg_retrieve_max_external_block(&pg, BlockNumber::ZERO, BlockNumber::MAX).await {
+        Ok(Some(block)) => block,
+        Ok(None) => BlockNumber::ZERO,
+        Err(e) => {
             cancellation.cancel();
-            break "error loading receipts";
+            return Err(e);
+        }
+    };
+    tracing::info!(%start, %end, "block limits");
+
+    // prepare loads to be executed in parallel
+    let mut tasks = Vec::new();
+    while start <= end {
+        let end = min(start + (BLOCKS_BY_FETCH - 1), end);
+        tasks.push(load_blocks_and_receipts(Arc::clone(&pg), cancellation.clone(), start, end));
+        start += BLOCKS_BY_FETCH;
+    }
+
+    // execute loads in parallel
+    let mut tasks = futures::stream::iter(tasks).buffered(paralellism);
+    let reason = loop {
+        // retrieve next batch of loaded blocks
+        let Some(result) = tasks.next().await else {
+            cancellation.cancel();
+            break "no more blocks to process";
         };
+
+        // check if executed correctly
+        let Ok((blocks, receipts)) = result else {
+            cancellation.cancel();
+            break "block or receipt fetch failed";
+        };
+
+        // check blocks were really loaded
+        if blocks.is_empty() {
+            cancellation.cancel();
+            return log_and_err!("no blocks returned when they were expected");
+        }
 
         // send to backlog
         if backlog.send((blocks, receipts)).await.is_err() {
+            tracing::error!("failed to send task to importer");
             cancellation.cancel();
-            break "error sending tasks to importer";
+            return log_and_err!("failed to send blocks and receipts to importer");
         };
     };
 
     tracing::info!(%reason, "postgres loader finished");
     Ok(())
+}
+
+async fn load_blocks_and_receipts(
+    pg: Arc<Postgres>,
+    cancellation: CancellationToken,
+    start: BlockNumber,
+    end: BlockNumber,
+) -> anyhow::Result<(Vec<BlockRow>, Vec<ReceiptRow>)> {
+    tracing::info!(%start, %end, "retrieving blocks and receipts");
+    if cancellation.is_cancelled() {
+        return Err(anyhow!("cancelled"));
+    }
+    let blocks_task = pg_retrieve_external_blocks_in_range(&pg, start, end);
+    let receipts_task = pg_retrieve_external_receipts_in_range(&pg, start, end);
+    try_join!(blocks_task, receipts_task)
 }
