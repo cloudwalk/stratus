8184cdfd3cbcc1321a85ea4ec531164cf190187a
Daniel Freire <daniel.freire@cloudwalk.io>
2024-03-01 17:40:00 -0300

    fmt

--- src/eth/primitives/block_header.rs
+++ src/eth/primitives/block_header.rs
@@ -34,7 +34,6 @@ const HASH_EMPTY_UNCLES: Hash = Hash::new(hex!("1dcc4de8dec75d7aab85b567b6ccd41a
 /// Special hash used in block mining to indicate no transaction root and no receipts root.
 const HASH_EMPTY_TRIE: Hash = Hash::new(hex!("56e81f171bcc55a6ff8345e692c0f86e5b48e01b996cadc001622fb5e363b421"));
 
-
 #[derive(Debug, Clone, PartialEq, Eq, serde::Serialize, serde::Deserialize)]
 pub struct BlockHeader {
     pub number: BlockNumber,
