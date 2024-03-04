3bc47d7bf0d601ec3464cdda69a3e291a77d1504
Renato Dinhani <renato.dinhani@cloudwalk.io>
2024-03-04 14:44:31 -0300

    feat: more metrics

--- src/infra/metrics.rs
+++ src/infra/metrics.rs
@@ -143,10 +143,10 @@ metrics! {
     histogram_duration evm_execution{point_in_time, success} [],
 
     "Number of accounts reads in a single EVM execution."
-    histogram_counter evm_execution_account_reads{} [0., 1., 2., 3., 4., 5.],
+    histogram_counter evm_execution_account_reads{} [0., 1., 2., 3., 4., 5., 6., 7., 8., 9., 10.],
 
     "Number of slot reads in a single EVM execution."
-    histogram_counter evm_execution_slot_reads{} [0., 10., 20., 30., 40., 50., 60., 70., 80., 90., 100.]
+    histogram_counter evm_execution_slot_reads{} [0., 5., 10., 15., 20., 25., 30., 35., 40., 45., 50., 55., 60., 65., 70., 75., 80., 85., 90., 95., 100.]
 }
 
 // -----------------------------------------------------------------------------
