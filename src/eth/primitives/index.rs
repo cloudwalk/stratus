6aced064aa4474c3f158c5b404b9cb66402e5e2d
Daniel Freire <daniel.freire@cloudwalk.io>
2024-03-05 14:24:59 -0300

    change Index internal type to u64

--- src/eth/primitives/index.rs
+++ src/eth/primitives/index.rs
@@ -6,6 +6,8 @@
 //! module includes functionality to handle conversions and representations of
 //! indexes, aligning with Ethereum's blockchain data structure needs.
 
+use std::num::TryFromIntError;
+
 use ethereum_types::U256;
 use ethereum_types::U64;
 use sqlx::database::HasArguments;
@@ -20,13 +22,13 @@ use crate::gen_newtype_from;
 ///
 /// TODO: representing it as u16 is probably wrong because external libs uses u64.
 #[derive(Debug, Clone, PartialEq, Eq, fake::Dummy, serde::Serialize, serde::Deserialize, derive_more::Add, Copy, Hash)]
-pub struct Index(u16);
+pub struct Index(u64);
 
 impl Index {
-    pub const ZERO: Index = Index(0u16);
-    pub const ONE: Index = Index(1u16);
+    pub const ZERO: Index = Index(0u64);
+    pub const ONE: Index = Index(1u64);
 
-    pub fn new(inner: u16) -> Self {
+    pub fn new(inner: u64) -> Self {
         Index(inner)
     }
 }
@@ -34,23 +36,23 @@ impl Index {
 // -----------------------------------------------------------------------------
 // Conversions: Other -> Self
 // -----------------------------------------------------------------------------
-gen_newtype_from!(self = Index, other = u16);
+gen_newtype_from!(self = Index, other = u64);
 
-impl From<i32> for Index {
-    fn from(value: i32) -> Self {
-        Index::new(value as u16)
+impl From<i64> for Index {
+    fn from(value: i64) -> Self {
+        Index::new(value as u64)
     }
 }
 
 impl From<U64> for Index {
     fn from(value: U64) -> Self {
-        Index::new(value.low_u64() as u16) // TODO: this will break things if the value is bigger than u16
+        Index::new(value.low_u64() as u64) // TODO: this will break things if the value is bigger than u16
     }
 }
 
 impl From<U256> for Index {
     fn from(value: U256) -> Self {
-        Index::new(value.low_u64() as u16) // TODO: this will break things if the value is bigger than u16
+        Index::new(value.low_u64() as u64) // TODO: this will break things if the value is bigger than u16
     }
 }
 
@@ -59,7 +61,7 @@ impl From<U256> for Index {
 // -----------------------------------------------------------------------------
 impl<'r> sqlx::Decode<'r, sqlx::Postgres> for Index {
     fn decode(value: <sqlx::Postgres as HasValueRef<'r>>::ValueRef) -> Result<Self, BoxDynError> {
-        let value = <i32 as sqlx::Decode<sqlx::Postgres>>::decode(value)?;
+        let value = <i64 as sqlx::Decode<sqlx::Postgres>>::decode(value)?;
         Ok(value.into())
     }
 }
@@ -67,19 +69,27 @@ impl<'r> sqlx::Decode<'r, sqlx::Postgres> for Index {
 impl sqlx::Type<sqlx::Postgres> for Index {
     fn type_info() -> <sqlx::Postgres as sqlx::Database>::TypeInfo {
         // HACK: Actually SERIAL, sqlx was panicking
-        sqlx::postgres::PgTypeInfo::with_name("INT4")
+        sqlx::postgres::PgTypeInfo::with_name("INT8")
     }
 }
 
 impl<'q> sqlx::Encode<'q, sqlx::Postgres> for Index {
     fn encode_by_ref(&self, buf: &mut <sqlx::Postgres as HasArguments<'q>>::ArgumentBuffer) -> IsNull {
-        <i32 as sqlx::Encode<sqlx::Postgres>>::encode(i32::from(self.0), buf)
+        let integer = match i64::try_from(self.0) {
+            Ok(val) => val,
+            Err(err) => {
+                tracing::error!(reason = ?err, "failed to convert Index to i64");
+                return IsNull::Yes
+            }
+        };
+
+        <i64 as sqlx::Encode<sqlx::Postgres>>::encode(integer, buf)
     }
 }
 
 impl PgHasArrayType for Index {
     fn array_type_info() -> sqlx::postgres::PgTypeInfo {
-        <i32 as PgHasArrayType>::array_type_info()
+        <i64 as PgHasArrayType>::array_type_info()
     }
 }
 
@@ -98,8 +108,10 @@ impl From<Index> for U256 {
     }
 }
 
-impl From<Index> for i32 {
-    fn from(value: Index) -> Self {
-        value.0.into()
+impl TryFrom<Index> for i32 {
+    type Error = TryFromIntError;
+
+    fn try_from(value: Index) -> Result<Self, Self::Error> {
+        value.0.try_into()
     }
 }
