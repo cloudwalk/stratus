6cbcfa30cf9f5ff48ad7865e3bc310bea396eb32
Renato Dinhani <renato.dinhani@cloudwalk.io>
2024-03-08 20:23:41 -0300

    fix: index.js

--- e2e/rpc-mock-server/index.js
+++ e2e/rpc-mock-server/index.js
@@ -5,12 +5,16 @@ const app = express();
 const port = 3003;
 
 // Load and parse CSV data synchronously
-const csvFilePath = './e2e/substrate-sync-mock-server/stratus_test_data.csv';
+const csvFilePath = './e2e/rpc-mock-server/stratus_test_data.csv';
 const csvContent = fs.readFileSync(csvFilePath, 'utf-8');
 const csvData = parse(csvContent, {
     columns: true,
     skip_empty_lines: true
 });
+for (const row of csvData) {
+    row.block_number = parseInt(row.block_number);
+}
+csvData.sort((a, b) => a.block_number - b.block_number);
 
 app.use(express.json());
 
@@ -18,42 +22,49 @@ console.log('Loaded', csvData.length, 'rows of data from', csvFilePath);
 app.post('/rpc', (req, res) => {
     console.log('Received RPC request:', req.body);
     try {
-        if (req.body.method === 'eth_getBlockByNumber') {
-            const blockNumberHex = req.body.params[0];
-            const blockNumber = parseInt(blockNumberHex, 16);
+        const method = req.body.method;
+        const response = {
+            jsonrpc: "2.0",
+            id: req.body.id
+        };
 
-            console.log('Requested block number:', blockNumber);
-                const matchingBlock = csvData.find(row => (row.block_number-1) === blockNumber);
-            if (matchingBlock && matchingBlock.block_payload) {
-                res.json({
-                    jsonrpc: "2.0",
-                    result: JSON.parse(matchingBlock.block_payload),
-                    id: req.body.id
-                });
-            } else {
-                res.status(404).json({ error: "Block not found" });
-            }
-        } else if (req.body.method === 'eth_getTransactionReceipt') {
-            const txHash = req.body.params[0];
+        switch(method) {
+            case "net_listening":
+                res.json({...response, result: true});
+                break;
 
-            const matchingReceipt = csvData.find(row => ('0x' + row.transaction_hash) === txHash);
-            if (matchingReceipt && matchingReceipt.receipt_payload) {
-                res.json({
-                    jsonrpc: "2.0",
-                    result: JSON.parse(matchingReceipt.receipt_payload),
-                    id: req.body.id
-                });
-            } else {
-                res.status(404).json({ error: "Receipt not found" });
-            }
-        } else if (req.body.method === 'eth_blockNumber') {
-            // Handler for 'eth_blockNumber'
-            const latestBlock = csvData.reduce((max, row) => Math.max(max, row.block_number), 0);
-            res.json({
-                jsonrpc: "2.0",
-                result: `0x${latestBlock.toString(16)}`, // Convert to hex string
-                id: req.body.id
-            });
+            case "eth_getBlockByNumber":
+                const blockNumberHex = req.body.params[0];
+                const blockNumber = parseInt(blockNumberHex, 16);
+
+                console.log('Requested block number:', blockNumber);
+                const matchingBlock = csvData.find(row => row.block_number === blockNumber);
+                if (matchingBlock && matchingBlock.block_payload) {
+                    console.log("Block found");
+                    res.json({...response, result: JSON.parse(matchingBlock.block_payload)});
+                } else {
+                    console.log("Block not found");
+                    res.json({...response, result: null})
+                }
+                break;
+
+            case "eth_getTransactionReceipt":
+                const txHash = req.body.params[0];
+
+                const matchingReceipt = csvData.find(row => ('0x' + row.transaction_hash) === txHash);
+                if (matchingReceipt && matchingReceipt.receipt_payload) {
+                    console.log("Receipt found");
+                    res.json({...response, result: JSON.parse(matchingReceipt.receipt_payload)})
+                } else {
+                    console.log("Receipt not found");
+                    res.json({...response, result: null})
+                }
+                break;
+
+            case "eth_blockNumber":
+                const latestBlock = csvData.reduce((max, row) => Math.max(max, row.block_number), 0);
+                res.json({...response, result: `0x${latestBlock.toString(16)}`});
+                break;
         }
     } catch (error) {
         console.error('Error processing request:', error);
