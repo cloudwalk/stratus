e1e68f2f1ac7d22540ca0d6e57236006523dd714
Maycon Amaro <maycon.amaro@cloudwalk.io>
2024-03-12 13:13:16 -0300

    test: add cashout

--- e2e-contracts/integration/test/integration.ts
+++ e2e-contracts/integration/test/integration.ts
@@ -46,7 +46,7 @@ describe("Integration Test", function () {
     let alice = ethers.Wallet.createRandom().connect(ethers.provider);
     let bob = ethers.Wallet.createRandom();
 
-    it("Mint 900 BRLC to Alice", async function () {
+    it("Mint BRLC to Alice", async function () {
       await brlCoin.connect(deployer).mint(alice.address, 900);
       expect(await brlCoin.balanceOf(alice.address)).to.equal(900);
     });
@@ -56,10 +56,20 @@ describe("Integration Test", function () {
       expect(await brlCoin.balanceOf(alice.address)).to.equal(1000);
     });
 
-    it("Alice transfers 50 BRLC to Bob", async function () {
+    it("Alice transfers BRLC to Bob", async function () {
       await brlCoin.connect(alice).transfer(bob.address, 50, { gasPrice: 0 });
       expect(await brlCoin.balanceOf(alice.address)).to.equal(950);
       expect(await brlCoin.balanceOf(bob.address)).to.equal(50);
+    });
+
+    it("Alice approves PixCashier to spend a lot of BRLC", async function () {
+      await brlCoin.connect(alice).approve(await pixCashier.getAddress(), 0xfffffffffffff, { gasPrice: 0 });
+      expect(await brlCoin.allowance(alice.address, await pixCashier.getAddress())).to.equal(0xfffffffffffff);
+    });
+
+    it("Pix request cashout subtracts from Alice's balance", async function () {
+      await pixCashier.connect(deployer).requestCashOutFrom(alice.address, 25, "0xabcdef1234567890abcdef1234567890abcdef1234567890abcdef1234567890");
+      expect(await brlCoin.balanceOf(alice.address)).to.equal(925);
     });
   });
 });
