c847a15cd00b5ce703455af9c9976c62402b6162
Maycon Amaro <maycon.amaro@cloudwalk.io>
2024-03-18 14:42:03 -0300

    chore: skip flattening if already made

--- e2e-contracts/flatten-contracts.sh
+++ e2e-contracts/flatten-contracts.sh
@@ -12,11 +12,17 @@ source $(dirname $0)/_functions.sh
 flatten() {
     repo=$1
     contract=$2
+
+    if [ -f integration/contracts/$contract.flattened.sol ]; then
+        log "Skipping flattening of $contract ($repo) (already exists)"
+        return
+    fi
+
     log "Flattenning: $contract ($repo)"
 
     # Enter the repository folder
     cd repos/$repo
-
+    
     # Flatten
     npx hardhat flatten contracts/$contract.sol > ../../integration/contracts/$contract.flattened.sol
     
@@ -26,6 +32,7 @@ flatten() {
     # Lint the flattened contract
     log "Linting the flattened $contract ($repo)"
     npx ts-node integration/test/helpers/lint-flattened.ts integration/contracts/$contract.flattened.sol
+    
 }
 
 # ------------------------------------------------------------------------------
