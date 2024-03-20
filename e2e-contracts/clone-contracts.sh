7be873c0a618ca4f492249e3f9eccd96efd0a2f5
Maycon Amaro <maycon.amaro@cloudwalk.io>
2024-03-20 13:29:58 -0300

    deps: update pix cashier to a528d0c

--- e2e-contracts/clone-contracts.sh
+++ e2e-contracts/clone-contracts.sh
@@ -46,4 +46,4 @@ clone brlc-periphery      b8d507a
 clone brlc-token          0858ec4
 clone compound-periphery  e4d68df
 clone brlc-yield-streamer e63d8ba
-clone brlc-pix-cashier    00cc007
+clone brlc-pix-cashier    a528d0c
