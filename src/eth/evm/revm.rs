b37c4b7bc5ef2013d08df3d24e2b4ca7dc0457fa
Renato Dinhani <renato.dinhani@cloudwalk.io>
2024-03-25 17:47:39 -0300

    tests

--- src/eth/evm/revm.rs
+++ src/eth/evm/revm.rs
@@ -57,6 +57,8 @@ pub struct Revm {
 impl Revm {
     /// Creates a new instance of the Revm ready to be used.
     pub fn new(storage: Arc<StratusStorage>, chain_id: ChainId) -> Self {
+        tracing::info!(%chain_id, "starting revm");
+
         let mut evm = RevmEvm::builder()
             .with_handler(Handler::mainnet_with_spec(SpecId::LONDON))
             .with_external_context(())
--- tests/test_execution.rs
+++ tests/test_execution.rs
@@ -2,37 +2,19 @@ use ethers_core::rand::thread_rng;
 // Adjust this import to include your Revm and related structs
 use fake::{Dummy, Faker};
 use stratus::config::CommonConfig;
-use stratus::config::MetricsHistogramKind;
-use stratus::config::PermanentStorageConfig;
-use stratus::config::PermanentStorageKind;
-use stratus::config::TemporaryStorageConfig;
-use stratus::config::TemporaryStorageKind;
 use stratus::eth::primitives::test_accounts;
 use stratus::eth::primitives::TransactionInput;
 use stratus::eth::primitives::Wei;
+use stratus::init_global_services;
 
 #[tokio::test]
 async fn test_execution() {
-    let config = CommonConfig {
-        perm_storage: PermanentStorageConfig {
-            perm_storage_kind: PermanentStorageKind::InMemory,
-            perm_storage_connections: 1,
-            perm_storage_timeout_millis: 1000,
-        },
-        temp_storage: TemporaryStorageConfig {
-            temp_storage_kind: TemporaryStorageKind::InMemory,
-        },
-        chain_id: 2008,
-        num_evms: 1usize,
-        num_async_threads: 1usize,
-        num_blocking_threads: 1usize,
-        metrics_histogram_kind: MetricsHistogramKind::Summary,
-        enable_genesis: false,
-        nocapture: false,
-    };
+    let config = init_global_services::<CommonConfig>();
+
     let storage = config.init_stratus_storage().await.unwrap();
     let mut rng = thread_rng();
     let mut fake_transaction_input = TransactionInput::dummy_with_rng(&Faker, &mut rng);
+    fake_transaction_input.chain_id = 2008u64.into();
     fake_transaction_input.nonce = 0u64.into();
     fake_transaction_input.gas_price = 0u64.into();
     fake_transaction_input.gas_limit = 0u64.into();
