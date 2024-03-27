d0031e04364f466558f5a5733d141f1ce9e67ce1
Maycon Amaro <maycon.amaro@cloudwalk.io>
2024-03-27 18:12:56 -0300

    test: add long versions in test-contracts

--- e2e-contracts/test-contracts.sh
+++ e2e-contracts/test-contracts.sh
@@ -42,24 +42,30 @@ test() {
 # configure tools
 asdf local nodejs 20.10.0
 
-while getopts tpmchix flag
-do
-    case "${flag}" in
-        t) token=1;;
-        p) periphery=1;;
-        m) multisig=1;;
-        c) compound=1;;
-        i) yield=1;;
-        x) pix=1;;
-        h) echo "Usage: test-contracts.sh [-[tpchix]]";
-           echo "-t for brlc-token";
-           echo "-p for brlc-periphery";
-           echo "-m for brlc-multisig";
-           echo "-c for compound-periphery";
-           echo "-i for brlc-yield-streamer";
-           echo "-x for brlc-pix-cashier";
-           echo "No parameter execute all tests";
-           exit;;
+# Função de ajuda
+print_help() {
+    echo "Usage: $0 [OPTIONS]"
+    echo "Options:"
+    echo "  -t, --token       for brlc-token"
+    echo "  -p, --periphery   for brlc-periphery"
+    echo "  -m, --multisig    for brlc-multisig"
+    echo "  -c, --compound    for compound-periphery"
+    echo "  -i, --yield       for brlc-yield-streamer"
+    echo "  -x, --pix         for brlc-pix-cashier"
+    echo "  -h, --help        display this help and exit"
+}
+
+# Processar argumentos
+while [[ "$#" -gt 0 ]]; do
+    case "$1" in
+        -h|--help) print_help; exit 0 ;;
+        -t|--token) token=1; shift ;;
+        -p|--periphery) periphery=1; shift ;;
+        -m|--multisig) multisig=1; shift ;;
+        -c|--compound) compound=1; shift ;;
+        -i|--yield) yield=1; shift ;;
+        -x|--pix) pix=1; shift ;;
+        *) echo "Unknown option: $1"; print_help; exit 1 ;;
     esac
 done
 
