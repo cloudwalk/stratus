eacdca9d13e0cd34c1fc61eee07b357b9d64a7d9
Renato Dinhani <101204870+dinhani-cw@users.noreply.github.com>
2024-03-28 15:41:36 -0300

    feat: initial support of .env files (#478)

--- src/lib.rs
+++ src/lib.rs
@@ -12,9 +12,25 @@ pub fn init_global_services<T>() -> T
 where
     T: clap::Parser + WithCommonConfig + Debug,
 {
+    // load .env files
+    let binary_full_path = std::env::current_exe().unwrap();
+    let mut binary = binary_full_path.file_name().unwrap().to_str().unwrap().to_owned();
+    if binary.starts_with("test_") {
+        binary = "test-int".to_string();
+    }
+    let env = std::env::var("ENV").unwrap_or_else(|_| "local".to_string());
+    let env_filename = format!("config/{}.env.{}", binary, env);
+
+    println!("Reading ENV file: {}", env_filename);
+    let _ = dotenvy::from_filename(env_filename);
+
+    // parse configuration
     let config = T::parse();
+    println!("Parsed configuration: {:?}", config);
+
+    // init services
     infra::init_tracing();
     infra::init_metrics(config.common().metrics_histogram_kind);
-    tracing::info!(?config);
+
     config
 }
