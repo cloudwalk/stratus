32a10ac44d405203c2fc2b53548c61bb85b6e219
Daniel Freire <daniel.freire@cloudwalk.io>
2024-04-09 18:05:09 -0300

    chore: remove hybrid storage

--- src/eth/storage/mod.rs
+++ src/eth/storage/mod.rs
@@ -2,7 +2,6 @@
 
 mod csv;
 mod external_rpc_storage;
-mod hybrid;
 mod inmemory;
 mod permanent_storage;
 mod postgres_external_rpc;
@@ -16,8 +15,6 @@ mod temporary_storage;
 
 pub use csv::CsvExporter;
 pub use external_rpc_storage::ExternalRpcStorage;
-pub use hybrid::HybridPermanentStorage;
-pub use hybrid::HybridPermanentStorageConfig;
 pub use inmemory::InMemoryPermanentStorage;
 pub use inmemory::InMemoryPermanentStorageState;
 pub use inmemory::InMemoryTemporaryStorage;
