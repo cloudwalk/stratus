8cb97ba919cda8bf74789f423f2be19a3f5b79e5
Maycon Amaro <maycon.amaro@cloudwalk.io>
2024-04-19 17:30:16 -0300

    revert justfiles and non-empty test files

--- tests/test_import_external_snapshot_postgres.rs
+++ tests/test_import_external_snapshot_postgres.rs
@@ -9,7 +9,6 @@ use stratus::infra::docker::Docker;
 use test_import_external_snapshot_common as common;
 
 #[tokio::test]
-#[ignore]
 async fn test_import_external_snapshot_with_postgres() {
     let docker = Docker::default();
     let _prom_guard = docker.start_prometheus();
