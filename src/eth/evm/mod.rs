09b3752166850d9030fe5e078d5815195ecd02f5
Renato Dinhani <101204870+dinhani-cw@users.noreply.github.com>
2024-04-22 09:56:20 -0300

    chore: upgrade to revm 8.0.0 (#643)

--- src/eth/evm/mod.rs
+++ src/eth/evm/mod.rs
@@ -1,15 +1,10 @@
-//! Ethereum Virtual Machine (EVM) Module
-//!
-//! This module serves as the backbone for executing Ethereum transactions in the Stratus project.
-//! It abstracts the EVM logic through the `Evm` trait and provides `EvmInput` for parameter encapsulation.
-//! Implementations of this trait, like `Revm`, offer concrete execution logic, leveraging dynamic dispatch
-//! via `Box<dyn Evm>` to allow for flexible, runtime-determined EVM operations.
-
 #[allow(clippy::module_inception)]
 mod evm;
+mod evm_error;
 pub mod revm;
 
 pub use evm::Evm;
 pub use evm::EvmExecutionResult;
 pub use evm::EvmInput;
 pub use evm::EvmInputSlotKeys;
+pub use evm_error::EvmError;
