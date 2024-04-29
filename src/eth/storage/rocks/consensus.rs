d3432ee61a5684660296c820f78d7f88023874e1
renancloudwalk <renan@cloudwalk.io>
2024-04-29 19:16:29 -0300

    chore: log block number

--- src/eth/storage/rocks/consensus.rs
+++ src/eth/storage/rocks/consensus.rs
@@ -4,6 +4,7 @@ use kube::api::Api;
 use kube::api::ListParams;
 use kube::api::ResourceExt;
 use kube::Client;
+use crate::infra::BlockchainClient;
 
 pub async fn gather_clients() -> Result<()> {
     // Infer the runtime environment and try to create a Kubernetes Client
@@ -18,6 +19,11 @@ pub async fn gather_clients() -> Result<()> {
         let pod_ip = pod.status.as_ref().unwrap().pod_ip.as_ref().unwrap().clone();
 
         println!("found pod {} with address {}", pod.name_any(), pod_ip);
+
+        let chain = BlockchainClient::new(&pod_ip).await.unwrap();
+        let block_number = chain.get_current_block_number().await.unwrap();
+
+        println!("block number: {}", block_number);
     }
     Ok(())
 }
