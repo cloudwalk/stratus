6ab38bca31cd48c966a866ffcf2437a3812be574
Daniel Freire <daniel.freire@cloudwalk.io>
2024-04-29 16:48:45 -0300

    refac

--- utils/compare_block/main.py
+++ utils/compare_block/main.py
@@ -6,6 +6,23 @@ import typer
 import web3
 from rich import print
 
+def print_items(changes: list, ignore: list[str]):
+    for change in changes:
+        field = change.path(root="", output_format="list")[0]
+        if field not in ignore:
+            print(f"\t\t[bold red]{field}[/bold red]")
+    print()
+
+def print_changed(changes: list, ignore: list[str]):
+    for change in changes:
+        field = change.path(root="", output_format="list")[0]
+        if field != "transactions" and field not in ignore:
+            if isinstance(change.up.t1[field], HexBytes) and isinstance(change.up.t2[field], HexBytes):
+                print(f"\t\t{field}: [bold red]Left: {change.up.t1[field].hex()}[/bold red], [bold green]Right: {change.up.t2[field].hex()}[/bold green]")
+            else:
+                print(f"\t\t{field}: [bold red]Left: {change.up.t1[field]}[/bold red], [bold green]Right: {change.up.t2[field]}[/bold green]")
+    print()
+
 def main(rpc_left: str, rpc_right: str, block: int, ignore: List[str] = []):
     w3_left = web3.Web3(web3.HTTPProvider(rpc_left))
     w3_right = web3.Web3(web3.HTTPProvider(rpc_right))
@@ -18,32 +35,15 @@ def main(rpc_left: str, rpc_right: str, block: int, ignore: List[str] = []):
     print("Block Header Diff:")
     if len(block_diff['dictionary_item_added']):
         print("\t[red]The left block is missing the following fields:[/red]")
-        for change in block_diff['dictionary_item_added']:
-            field = change.path(root="", output_format="list")[0]
-            if field != "transactions" and field not in ignore:
-                print(f"\t\t[bold red]{field}[/bold red]")
-        print()
-
+        print_items(block_diff['dictionary_item_added'], [*ignore, "transactions"])
 
     if len(block_diff['dictionary_item_removed']):
         print("\t[red]The right block is missing the following fields:[/red]")
-        for change in block_diff['dictionary_item_removed']:
-            field = change.path(root="", output_format="list")[0]
-            if field != "transactions" and field not in ignore:
-                print(f"[bold red]\t\t{field}[/bold red]")
-        print()
+        print_items(block_diff['dictionary_item_removed'], [*ignore, "transactions"])
 
     if len(block_diff['values_changed']):
         print("\t[red]The follwing values don't match:[/red]")
-        for change in block_diff['values_changed']:
-            field = change.path(root="", output_format="list")[0]
-            if field != "transactions" and field not in ignore:
-                if isinstance(block_left[field], HexBytes) and isinstance(block_right[field], HexBytes):
-                    print(f"\t\t{field}: [bold red]Left: {block_left[field].hex()}[/bold red], [bold green]Right: {block_right[field].hex()}[/bold green]")
-                else:
-                    print(f"\t\t{field}: [bold red]Left: {block_left[field]}[/bold red], [bold green]Right: {block_right[field]}[/bold green]")
-        print()
-
+        print_changed(block_diff['values_changed'], [*ignore, "transactions"])
 
     transactions_left = block_left.get('transactions')
     transactions_right = block_right.get('transactions')
@@ -59,31 +59,15 @@ def main(rpc_left: str, rpc_right: str, block: int, ignore: List[str] = []):
 
             if len(diff.get('dictionary_item_added', [])):
                 print("\t\t[red]The left tx is missing the following fields:[/red]")
-                for change in diff['dictionary_item_added']:
-                    field = change.path(root="", output_format="list")[0]
-                    if field not in ignore:
-                        print(f"\t\t\t[bold red]{field}[/bold red]")
-                print()
-
+                print_items(diff['dictionary_item_added'], ignore)
 
             if len(diff.get('dictionary_item_removed', [])):
                 print("\t\t[red]The right tx is missing the following fields:[/red]")
-                for change in diff['dictionary_item_removed']:
-                    field = change.path(root="", output_format="list")[0]
-                    if field not in ignore:
-                        print(f"[bold red]\t\t\t{field}[/bold red]")
-                print()
+                print_items(diff['dictionary_item_removed'], ignore)
 
             if len(diff.get('values_changed', [])):
                 print("\t\t[red]The follwing values don't match:[/red]")
-                for change in diff['values_changed']:
-                    field = change.path(root="", output_format="list")[0]
-                    if field not in ignore:
-                        if isinstance(left[field], HexBytes) and isinstance(right[field], HexBytes):
-                            print(f"\t\t\t{field}: [bold red]Left: {left[field].hex()}[/bold red], [bold green]Right: {right[field].hex()}[/bold green]")
-                        else:
-                            print(f"\t\t\t{field}: [bold red]Left: {left[field]}[/bold red], [bold green]Right: {right[field]}[/bold green]")
-                print()
+                print_changed(diff['values_changed'], [*ignore, "transactions"])
 
             if not isinstance(left, HexBytes) and not isinstance(right, HexBytes):
                 print(f"\tTransaction {idx} Receipt Diff:")
@@ -95,31 +79,15 @@ def main(rpc_left: str, rpc_right: str, block: int, ignore: List[str] = []):
 
                 if len(diff.get('dictionary_item_added', [])):
                     print("\t\t[red]The left tx receipt is missing the following fields:[/red]")
-                    for change in diff['dictionary_item_added']:
-                        field = change.path(root="", output_format="list")[0]
-                        if field != "logs" and field not in ignore:
-                            print(f"\t\t\t[bold red]{field}[/bold red]")
-                    print()
-
+                    print_items(diff['dictionary_item_added'], [*ignore, "logs"])
 
                 if len(diff.get('dictionary_item_removed', [])):
                     print("\t\t[red]The right tx receipt is missing the following fields:[/red]")
-                    for change in diff['dictionary_item_removed']:
-                        field = change.path(root="", output_format="list")[0]
-                        if field != "logs" and field not in ignore:
-                            print(f"[bold red]\t\t\t{field}[/bold red]")
-                    print()
+                    print_items(diff['dictionary_item_removed'], [*ignore, "logs"])
 
                 if len(diff.get('values_changed', [])):
                     print("\t\t[red]The follwing values don't match:[/red]")
-                    for change in diff['values_changed']:
-                        field = change.path(root="", output_format="list")[0]
-                        if field not in ignore:
-                            if isinstance(left[field], HexBytes) and isinstance(right[field], HexBytes):
-                                print(f"\t\t\t{field}: [bold red]Left: {left[field].hex()}[/bold red], [bold green]Right: {right[field].hex()}[/bold green]")
-                            else:
-                                print(f"\t\t\t{field}: [bold red]Left: {pformat(left[field], indent=8)}[/bold red], [bold green]Right: {pformat(right[field], indent=8)}[/bold green]")
-                    print()
+                    print_changed(diff['values_changed'], ignore)
 
 
 if __name__ == "__main__":
