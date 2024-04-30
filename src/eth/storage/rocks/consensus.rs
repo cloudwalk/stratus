76ccdf02dfe0facde99debce6b406284d5df9ce9
renancloudwalk <renan@cloudwalk.io>
2024-04-30 08:51:03 -0300

    chore: add dummy pod election

--- src/eth/storage/rocks/consensus.rs
+++ src/eth/storage/rocks/consensus.rs
@@ -1,3 +1,7 @@
+//TODO move this onto temporary storage, it will be called from a channel
+use rand::thread_rng;
+use rand::Rng;
+use std::collections::HashMap;
 use anyhow::Result;
 use k8s_openapi::api::core::v1::Pod;
 use kube::api::Api;
@@ -16,10 +20,32 @@ pub async fn gather_clients() -> Result<()> {
     // Read pods in the configured namespace into the typed interface from k8s-openapi
     let pods: Api<Pod> = Api::default_namespaced(client);
     let pods_list = pods.list(&ListParams::default()).await.unwrap();
-    for pod in pods_list {
+
+    // Choose a random pod to be the leader
+    let mut rng = thread_rng();
+    let leader_index = rng.gen_range(0..pods_list.items.len());
+    let leader_pod = &pods_list.items[leader_index];
+
+    // Initialize a HashMap to store pod IPs and roles
+    let mut pod_roles: HashMap<String, String> = HashMap::new();
+
+    for (index, pod) in pods_list.items.iter().enumerate() {
+        let role = if index == leader_index { "leader" } else { "follower" };
         let pod_ip = format!("http://{}:3000", pod.status.as_ref().unwrap().pod_ip.as_ref().unwrap().clone());
 
         println!("found pod {} with address {}", pod.name_any(), pod_ip);
+
+        let patch = json!({
+            "metadata": {
+                "labels": {
+                    "role": role
+                }
+            }
+        });
+
+        let pp = PatchParams::apply("leader-election-handler");
+
+        pods.patch(&pod.metadata.name.as_ref().unwrap(), &pp, &Patch::Apply(&patch)).await?;
 
         if pod.name_any() != std::env::var("HOSTNAME")? {
             let chain = BlockchainClient::new(&pod_ip).await.unwrap();
