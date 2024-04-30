80bc1f21a7bad3cf13b68d2b35aac2b1cd610b8a
renancloudwalk <53792026+renancloudwalk@users.noreply.github.com>
2024-04-29 22:55:43 -0300

    Pods ip mapping (#728)
    * chore: add raft service account
    
    * chore: gather pods ip
    
    * fix: typo
    
    * temporarily remove serviceAccount
    
    * chore: log block number
    
    * fix: add missing http
    
    * fix: add missing port

--- src/eth/storage/rocks/consensus.rs
+++ src/eth/storage/rocks/consensus.rs
@@ -5,6 +5,8 @@ use kube::api::ListParams;
 use kube::api::ResourceExt;
 use kube::Client;
 
+use crate::infra::BlockchainClient;
+
 pub async fn gather_clients() -> Result<()> {
     // Infer the runtime environment and try to create a Kubernetes Client
     let client = Client::try_default().await.unwrap();
@@ -13,8 +15,16 @@ pub async fn gather_clients() -> Result<()> {
 
     // Read pods in the configured namespace into the typed interface from k8s-openapi
     let pods: Api<Pod> = Api::default_namespaced(client);
-    for p in pods.list(&ListParams::default()).await.unwrap() {
-        println!("found pod {}", p.name_any());
+    let pods_list = pods.list(&ListParams::default()).await.unwrap();
+    for pod in pods_list {
+        let pod_ip = format!("http://{}:3000", pod.status.as_ref().unwrap().pod_ip.as_ref().unwrap().clone());
+
+        println!("found pod {} with address {}", pod.name_any(), pod_ip);
+
+        let chain = BlockchainClient::new(&pod_ip).await.unwrap();
+        let block_number = chain.get_current_block_number().await.unwrap();
+
+        println!("block number: {}", block_number);
     }
     Ok(())
 }
