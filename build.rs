cd6c60cdd1ec4b571549cd5b61a18ef80e57675e
renancloudwalk <renan@cloudwalk.io>
2024-05-04 23:39:52 -0300

    Revert "Tonic (#775)"
    This reverts commit 4c3510da26deb3a7faba16c3fd0149a1a558855a.

--- build.rs
+++ build.rs
@@ -14,7 +14,6 @@ use nom::IResult;
 
 fn main() {
     generate_signature_maps();
-    tonic_build::compile_protos("proto/management.proto").unwrap();
 }
 
 // -----------------------------------------------------------------------------
