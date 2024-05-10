b8ba4b2d3da7d86bccc0985ab726867de74e5e47
Daniel Freire <daniel.freire@cloudwalk.io>
2024-05-10 12:18:18 -0300

    cancel if handler creation fails

--- src/utils.rs
+++ src/utils.rs
@@ -1,5 +1,7 @@
+use anyhow::anyhow;
 use tokio::select;
 use tokio::signal::unix::signal;
+use tokio::signal::unix::Signal;
 use tokio::signal::unix::SignalKind;
 use tokio_util::sync::CancellationToken;
 use uuid::Uuid;
@@ -8,12 +10,29 @@ pub fn new_context_id() -> String {
     Uuid::new_v4().to_string()
 }
 
+fn signal_or_cancel(kind: SignalKind, cancellation: &CancellationToken) -> anyhow::Result<Signal> {
+    match signal(kind) {
+        Ok(signal) => Ok(signal),
+        Err(err) => {
+            tracing::error!(?err, "unable to listen for SIGTERM");
+            cancellation.cancel();
+            Err(anyhow!("signal handler init failed"))
+        }
+    }
+}
+
 pub fn signal_handler() -> CancellationToken {
     let cancellation = CancellationToken::new();
     let task_cancellation = cancellation.clone();
     tokio::spawn(async move {
-        let mut sigterm = signal(SignalKind::terminate()).expect("unable to listen for SIGTERM");
-        let mut sigint = signal(SignalKind::interrupt()).expect("unable to listen for SIGTERM");
+        let Ok(mut sigterm) = signal_or_cancel(SignalKind::terminate(), &task_cancellation) else {
+            return;
+        };
+
+        let Ok(mut sigint) = signal_or_cancel(SignalKind::interrupt(), &task_cancellation) else {
+            return;
+        };
+
         select! {
             _ = sigterm.recv() => {
                 tracing::info!("SIGTERM received, cancelling tasks");
