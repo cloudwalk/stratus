2efc8407b6134cf98fb0e1e4028e695287c9063d
gabriel-aranha-cw <gabriel.aranha@cloudwalk.io>
2024-06-05 09:54:58 -0300

    improve tests

--- e2e-contracts/integration/test/brlc.test.ts
+++ e2e-contracts/integration/test/brlc.test.ts
@@ -1,5 +1,6 @@
 import { ethers } from "hardhat";
 import { expect } from "chai";
+import { getDbClient } from './helpers/db';
 import {
     GAS_LIMIT_OVERRIDE,
     brlcToken,
@@ -27,118 +28,136 @@ describe("Relayer integration test", function () {
 
     });
     describe("Transaction tests", function () {
-        let alice = ethers.Wallet.createRandom().connect(ethers.provider);
-        let bob = ethers.Wallet.createRandom().connect(ethers.provider);
+        const parameters = [
+            { wallets: 2, duration: 10, tps: 5 },
+            { wallets: 100, duration: 10, tps: 30 },
+        ];
+        parameters.forEach((params, index) => {
+            const wallets: any[] = [];
+            it(`Test case ${index + 1}: Prepare and mint BRLC to wallets`, async function () {  
+                this.timeout(params.wallets * 1000 + 10000);
 
-        it("Validate mint BRLC to wallets", async function () {      
-            expect(await await ethers.provider.getBalance(alice.address)).to.be.equal(0);
-            expect(await await ethers.provider.getBalance(bob.address)).to.be.equal(0);
-
-            expect(await brlcToken.mint(alice.address, 2000, { gasLimit: GAS_LIMIT_OVERRIDE })).to.have.changeTokenBalance(brlcToken, alice, 2000);
-            expect(await brlcToken.mint(bob.address, 2000, { gasLimit: GAS_LIMIT_OVERRIDE })).to.have.changeTokenBalance(brlcToken, bob, 2000);
-
-            expect(await brlcToken.balanceOf(alice.address)).to.equal(2000);
-            expect(await brlcToken.balanceOf(bob.address)).to.equal(2000);
-        });
-
-        let txHashList: string[] = []
-        it("Transfer BRLC between wallets at a configurable TPS", async function () {
-            const testDuration = 10;
-            this.timeout(testDuration * 1000 + 10000);
-
-            const TPS = 5;
-            const wallets = [alice, bob];
-            const transactionInterval = 1000 / TPS;
-
-            let nonces = await Promise.all(wallets.map(wallet => send("eth_getTransactionCount", [wallet.address, "latest"])));
-
-            const startTime = Date.now();
-            while (Date.now() - startTime < testDuration * 1000) {
-                let senderIndex;
-                let receiverIndex;
-                do {
-                    senderIndex = Math.floor(Math.random() * wallets.length);
-                    receiverIndex = Math.floor(Math.random() * wallets.length);
-                } while (senderIndex === receiverIndex);
-
-                let sender = wallets[senderIndex];
-                let receiver = wallets[receiverIndex];
-
-                const amount = Math.floor(Math.random() * 10) + 1;
-
-                const tx = await brlcToken.connect(sender).transfer(receiver.address, amount, { gasPrice: 0, gasLimit: GAS_LIMIT_OVERRIDE, type: 0, nonce: nonces[senderIndex] });
-                txHashList.push(tx.hash);
-
-                nonces[senderIndex]++;
-
-                await new Promise(resolve => setTimeout(resolve, transactionInterval));
-            }
-            await new Promise(resolve => setTimeout(resolve, 5000));
-        });
-
-        it("Validate transaction mined delay between Stratus and Hardhat", async function () {
-            // Get Stratus timestamps
-            updateProviderUrl("stratus");
-            const stratusTimestamps = await Promise.all(txHashList.map(async (txHash) => {
-                const receipt = await send("eth_getTransactionReceipt", [txHash]);
-                const block = await send("eth_getBlockByNumber", [receipt.blockNumber, false]);
-                return parseInt(block.timestamp, 16);
-            }));
-
-            // Get Hardhat timestamps
-            updateProviderUrl("hardhat");
-            const hardhatTimestamps = await Promise.all(txHashList.map(async (txHash) => {
-                const receipt = await send("eth_getTransactionReceipt", [txHash]);
-                const block = await send("eth_getBlockByNumber", [receipt.blockNumber, false]);
-                return parseInt(block.timestamp, 16);
-            }));
-
-            // Total time it took for Stratus to process all the blocks containing transactions
-            const stratusProcessingTime = stratusTimestamps[stratusTimestamps.length - 1] - stratusTimestamps[0];
-
-            // Total time it took for Hardhat to process all the blocks containing transactions
-            const hardhatProcessingTime = hardhatTimestamps[hardhatTimestamps.length - 1] - hardhatTimestamps[0];
-
-            console.log(`WARN: Stratus took ${stratusProcessingTime} seconds to process all the blocks`);
-            console.log(`WARN: Hardhat took ${hardhatProcessingTime} seconds to process all the blocks`);
-        });
-
-        it("Validate balances between Stratus and Hardhat", async function () {
-            // Get Stratus balances
-            updateProviderUrl("stratus");
-            const stratusAliceBalance = await brlcToken.balanceOf(alice.address);
-            const stratusBobBalance = await brlcToken.balanceOf(bob.address);
-
-            // Get Hardhat balances
-            updateProviderUrl("hardhat");
-            const hardhatAliceBalance = await brlcToken.balanceOf(alice.address);
-            const hardhatBobBalance = await brlcToken.balanceOf(bob.address);
-
-            // Assert that the balances are equal
-            expect(stratusAliceBalance).to.equal(hardhatAliceBalance, "Alice balances are not equal between Stratus and Hardhat");
-            expect(stratusBobBalance).to.equal(hardhatBobBalance, "Alice balances are not equal between Stratus and Hardhat");
-        });
-
-        it("Validate transactions were relayed from Stratus to Hardhat", async function () {
-            // Get Stratus transaction receipts
-            updateProviderUrl("stratus");
-            const stratusReceipts = await Promise.all(txHashList.map(async (txHash) => {
-                const receipt = await send("eth_getTransactionReceipt", [txHash]);
-                return receipt;
-            }));
-
-            // Get Hardhat transaction receipts
-            updateProviderUrl("hardhat");
-            const hardhatReceipts = await Promise.all(txHashList.map(async (txHash) => {
-                const receipt = await send("eth_getTransactionReceipt", [txHash]);
-                return receipt;
-            }));
-
-            // Assert that all transactions were relayed
-            for (let i = 0; i < txHashList.length; i++) {
-                expect(stratusReceipts[i]).to.exist;
-                expect(hardhatReceipts[i]).to.exist;
-            }
+                for (let i = 0; i < params.wallets; i++) {
+                    const wallet = ethers.Wallet.createRandom().connect(ethers.provider);
+                    wallets.push(wallet);
+                }
+    
+                for (let i = 0; i < wallets.length; i++) {
+                    const wallet = wallets[i];
+                    expect(await ethers.provider.getBalance(wallet.address)).to.be.equal(0);
+                    expect(await brlcToken.mint(wallet.address, 2000, { gasLimit: GAS_LIMIT_OVERRIDE })).to.have.changeTokenBalance(brlcToken, wallet, 2000);
+                    expect(await brlcToken.balanceOf(wallet.address)).to.equal(2000);
+                }
+            });
+    
+            let txHashList: string[] = []
+            it(`Test case ${index + 1}: Transfer BRLC between wallets at a configurable TPS`, async function () {
+                this.timeout(params.duration * 1000 + 10000);
+    
+                const transactionInterval = 1000 / params.tps;
+    
+                let nonces = await Promise.all(wallets.map(wallet => send("eth_getTransactionCount", [wallet.address, "latest"])));
+    
+                const startTime = Date.now();
+                while (Date.now() - startTime < params.duration * 1000) {
+                    let senderIndex;
+                    let receiverIndex;
+                    do {
+                        senderIndex = Math.floor(Math.random() * wallets.length);
+                        receiverIndex = Math.floor(Math.random() * wallets.length);
+                    } while (senderIndex === receiverIndex);
+    
+                    let sender = wallets[senderIndex];
+                    let receiver = wallets[receiverIndex];
+    
+                    const amount = Math.floor(Math.random() * 10) + 1;
+    
+                    const tx = await brlcToken.connect(sender).transfer(receiver.address, amount, { gasPrice: 0, gasLimit: GAS_LIMIT_OVERRIDE, type: 0, nonce: nonces[senderIndex] });
+                    txHashList.push(tx.hash);
+    
+                    nonces[senderIndex]++;
+    
+                    await new Promise(resolve => setTimeout(resolve, transactionInterval));
+                }
+                await new Promise(resolve => setTimeout(resolve, 5000));
+            });
+    
+            it(`Test case ${index + 1}: Validate transaction mined delay between Stratus and Hardhat`, async function () {
+                // Get Stratus timestamps
+                updateProviderUrl("stratus");
+                const stratusTimestamps = await Promise.all(txHashList.map(async (txHash) => {
+                    const receipt = await send("eth_getTransactionReceipt", [txHash]);
+                    const block = await send("eth_getBlockByNumber", [receipt.blockNumber, false]);
+                    return parseInt(block.timestamp, 16);
+                }));
+    
+                // Get Hardhat timestamps
+                updateProviderUrl("hardhat");
+                const hardhatTimestamps = await Promise.all(txHashList.map(async (txHash) => {
+                    const receipt = await send("eth_getTransactionReceipt", [txHash]);
+                    const block = await send("eth_getBlockByNumber", [receipt.blockNumber, false]);
+                    return parseInt(block.timestamp, 16);
+                }));
+    
+                // Total time it took for Stratus to process all the blocks containing transactions
+                const stratusProcessingTime = stratusTimestamps[stratusTimestamps.length - 1] - stratusTimestamps[0];
+    
+                // Total time it took for Hardhat to process all the blocks containing transactions
+                const hardhatProcessingTime = hardhatTimestamps[hardhatTimestamps.length - 1] - hardhatTimestamps[0];
+    
+                console.log(`WARN: Stratus processing time: ${stratusProcessingTime}s | Hardhat processing time: ${hardhatProcessingTime}s`);
+            });
+    
+            it(`Test case ${index + 1}: Validate balances between Stratus and Hardhat`, async function () {
+                for (let i = 0; i < wallets.length; i++) {
+                    // Get Stratus balance
+                    updateProviderUrl("stratus");
+                    const stratusBalance = await brlcToken.balanceOf(wallets[i].address);
+    
+                    // Get Hardhat balance
+                    updateProviderUrl("hardhat");
+                    const hardhatBalance = await brlcToken.balanceOf(wallets[i].address);
+    
+                    // Assert that the balances are equal
+                    expect(stratusBalance).to.equal(hardhatBalance, `Wallet ${wallets[i].address} balances are not equal between Stratus and Hardhat`);
+                }
+            });
+    
+            it(`Test case ${index + 1}: Validate transactions were relayed from Stratus to Hardhat`, async function () {
+                // Get Stratus transaction receipts
+                updateProviderUrl("stratus");
+                const stratusReceipts = await Promise.all(txHashList.map(async (txHash) => {
+                    const receipt = await send("eth_getTransactionReceipt", [txHash]);
+                    return receipt;
+                }));
+    
+                // Get Hardhat transaction receipts
+                updateProviderUrl("hardhat");
+                const hardhatReceipts = await Promise.all(txHashList.map(async (txHash) => {
+                    const receipt = await send("eth_getTransactionReceipt", [txHash]);
+                    return receipt;
+                }));
+    
+                // Assert that all transactions were relayed
+                for (let i = 0; i < txHashList.length; i++) {
+                    expect(stratusReceipts[i]).to.exist;
+                    expect(hardhatReceipts[i]).to.exist;
+                }
+            });
+    
+            it(`Test case ${index + 1}: Validate no mismatched transactions were generated`, async function () {
+                const client = await getDbClient();
+            
+                // Validate connection to Postgres
+                const testRes = await client.query('SELECT 1');
+                expect(testRes.rowCount).to.equal(1, 'Could not connect to the database');
+            
+                // Validate that no mismatched transactions were found
+                const res = await client.query('SELECT * FROM mismatches');
+                expect(res.rows.length).to.equal(0, 'Mismatched transactions were found');
+            
+                await client.end();
+            });
         });
     });
 });
