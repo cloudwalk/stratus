e4f1dce7971c7dcac0f090e7906cca3dc9614547
Maycon Amaro <maycon.amaro@cloudwalk.io>
2024-06-11 12:57:17 -0300

    ignore bytecode test

--- src/eth/primitives/bytecode.rs
+++ src/eth/primitives/bytecode.rs
@@ -96,6 +96,7 @@ mod tests {
     const BYTECODE_PIX: &str = include_str!("../../../tests/fixtures/bytecodes/PixCashier.bin");
 
     #[test]
+    #[ignore] //HACK: remove the entire feature instead of ignoring the test
     // TODO: add assertions based on storage layout file generated from source code.
     fn parse_bytecode_slots() {
         // brlc snapshot
