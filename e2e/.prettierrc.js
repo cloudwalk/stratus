fea2c330f2fd7eb7f6b73a80011b2e6db8962a7d
Evgenii Zaitsev <evgenii@cloudwalk.io>
2024-06-11 19:53:12 +0700

    chore: introduce the Prettier plugin for the Solidity files

--- e2e/.prettierrc.js
+++ e2e/.prettierrc.js
@@ -1,5 +1,5 @@
 module.exports = {
-    plugins: ["@trivago/prettier-plugin-sort-imports"],
+    plugins: ["@trivago/prettier-plugin-sort-imports", "prettier-plugin-solidity"],
     // lines
     printWidth: 120,
     tabWidth: 4,
@@ -14,4 +14,19 @@ module.exports = {
     importOrder: ["<THIRD_PARTY_MODULES>", "^[./]"],
     importOrderSeparation: true,
     importOrderSortSpecifiers: true,
+
+    overrides: [
+        // Solidity
+        {
+            files: "*.sol",
+            options: {
+                parser: "solidity-parse",
+                printWidth: 120,
+                tabWidth: 4,
+                useTabs: false,
+                singleQuote: false,
+                bracketSpacing: true,
+            },
+        },
+    ],
 };
