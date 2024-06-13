d4084ad0ec8be2ce71ad3bfd163800a5fb34bed0
gabriel-aranha-cw <gabriel.aranha@cloudwalk.io>
2024-06-13 10:08:06 -0300

    hardcode to simplify params

--- chaos/experiments/leader-election.sh
+++ chaos/experiments/leader-election.sh
@@ -48,60 +48,47 @@ check_leader() {
 # Function to find the leader
 find_leader() {
     local ports=("$@")
-    local max_retries=10
-    local retry_interval=5
-    local retries=0
-    while [ $retries -lt $max_retries ]; do
-        for port in "${ports[@]}"; do
-            result=$(check_leader $port)
-            if [ "$result" = "true" ]; then
-                echo $port
-                return
-            fi
-        done
-        retries=$((retries + 1))
-        sleep $retry_interval
+    local leaders=()
+    for port in "${ports[@]}"; do
+        result=$(check_leader $port)
+        if [ "$result" = "true" ]; then
+            leaders+=($port)
+        fi
     done
-    echo ""
+    echo "${leaders[@]}"
+}
+
+# Function to check the leader status and handle errors
+check_leader_status() {
+    local leader_ports=("$@")
+    if [ ${#leader_ports[@]} -gt 1 ]; then
+        # Error: More than one leader found
+        return 1
+    elif [ ${#leader_ports[@]} -eq 0 ]; then
+        # Error: No leader found
+        return 1
+    else
+        echo "Leader found on port ${leader_ports[0]}"
+        return 0
+    fi
 }
 
 # Function to remove rocks-path directory
 remove_rocks_path() {
     local rocks_path=$1
-    echo "Removing rocks-path directory $rocks_path"
     rm -rf $rocks_path
 }
 
 # Function to run the election test
 run_test() {
-    local num_instances=$1
-    local instances=()
-
-    # Prepare instance parameters
-    for ((i=1; i<=num_instances; i++)); do
-        local port=$((3000 + i))
-        local grpc_port=$((3777 + i))
-        local tokio_console_port=$((4000 + i))
-        local metrics_exporter_port=$((5000 + i))
-        local tmp_dir="tmp_rocks_$port"
-        local log_file="instance_$port.log"
-
-        # Generate candidate peers string
-        candidate_peers=""
-        for ((j=1; j<=num_instances; j++)); do
-            if [ $i -ne $j ]; then
-                if [ -n "$candidate_peers" ]; then
-                    candidate_peers="$candidate_peers,"
-                fi
-                candidate_peers="$candidate_peers""http://0.0.0.0:$((3000 + j));$((3777 + j))"
-            fi
-        done
-
-        instances+=("0.0.0.0:$port 0.0.0.0:$grpc_port $tmp_dir $log_file $port $candidate_peers 0.0.0.0:$tokio_console_port 0.0.0.0:$metrics_exporter_port")
-    done
+    local instances=(
+        "0.0.0.0:3001 0.0.0.0:3778 tmp_rocks_3001 instance_3001.log 3001 http://0.0.0.0:3002;3779,http://0.0.0.0:3003;3780 0.0.0.0:6669 0.0.0.0:9001"
+        "0.0.0.0:3002 0.0.0.0:3779 tmp_rocks_3002 instance_3002.log 3002 http://0.0.0.0:3001;3778,http://0.0.0.0:3003;3780 0.0.0.0:6670 0.0.0.0:9002"
+        "0.0.0.0:3003 0.0.0.0:3780 tmp_rocks_3003 instance_3003.log 3003 http://0.0.0.0:3001;3778,http://0.0.0.0:3002;3779 0.0.0.0:6671 0.0.0.0:9003"
+    )
 
     # Start instances
-    echo "Starting $num_instances instances..."
+    echo "Starting 3 instances..."
     pids=()
     ports=()
     rocks_paths=()
@@ -130,97 +117,116 @@ run_test() {
         done
         if [ "$all_ready" != true ]; then
             echo "Waiting for all instances to be ready..."
-            sleep 5 # Wait for a bit before checking again
+            sleep 5
         fi
     done
 
-    echo "All instances are ready."
+    echo "All instances are ready. Waiting for leader election"
 
-    # Check which instance is the leader
-    leader_port=$(find_leader "${ports[@]}")
-    if [ -n "$leader_port" ]; then
-        echo "Leader found on port $leader_port"
+    # Maximum timeout duration in seconds for the initial leader election
+    initial_leader_timeout=60
 
-        # Kill the leader instance
-        echo "Killing the leader instance on port $leader_port..."
-        for i in "${!ports[@]}"; do
-            if [ "${ports[i]}" -eq "$leader_port" ]; then
-                kill ${pids[i]}
-                unset ports[i]
-                unset pids[i]
-                break
-            fi
-        done
+    # Capture the start time
+    initial_start_time=$(date +%s)
 
-        sleep 5
+    # Wait for the initial leader election with a timeout
+    while true; do
+        current_time=$(date +%s)
+        elapsed_time=$((current_time - initial_start_time))
 
-        # Restart the killed instance
-        echo "Restarting the killed instance..."
-        for instance in "${instances[@]}"; do
-            IFS=' ' read -r -a params <<< "$instance"
-            if [ "${params[4]}" -eq "$leader_port" ]; then
-                new_pid=$(start_instance "${params[0]}" "${params[1]}" "${params[2]}" "${params[3]}" "${params[5]}" "${params[6]}" "${params[7]}")
-                pids+=("$new_pid")
-                ports+=("${params[4]}")
-                readiness+=(false)
-                break
-            fi
-        done
+        if [ $elapsed_time -ge $initial_leader_timeout ]; then
+            echo "Timeout reached without finding an initial leader."
+            break
+        fi
 
-        all_ready=false
-        while [ "$all_ready" != true ]; do
-            all_ready=true
-            for i in "${!ports[@]}"; do
-                if [ "${readiness[$i]}" != true ]; then
-                    response=$(check_liveness "${ports[$i]}")
-                    if [ "$response" = "true" ]; then
-                        readiness[$i]=true
-                        echo "Instance on port ${ports[$i]} is ready."
-                    else
-                        all_ready=false
-                    fi
-                fi
-            done
-            if [ "$all_ready" != true ]; then
-                echo "Waiting for all instances to be ready..."
-                sleep 5 # Wait for a bit before checking again
-            fi
-        done
+        leader_ports=($(find_leader "${ports[@]}"))
+        if check_leader_status "${leader_ports[@]}"; then
+            leader_port=${leader_ports[0]}
+            break
+        else
+            sleep 1
+        fi
+    done
 
-        echo "All instances are ready after restart."
-
-        # Maximum timeout duration in seconds for new leader election
-        max_timeout=30
-
-        # Capture the start time
-        start_time=$(date +%s)
-
-        # Wait until a new leader is found or timeout
-        while true; do
-            current_time=$(date +%s)
-            elapsed_time=$((current_time - start_time))
-
-            if [ $elapsed_time -ge $max_timeout ]; then
-                echo "Timeout reached without finding a new leader."
-                break # Exit the loop due to timeout
-            fi
-
-            new_leader_port=$(find_leader "${ports[@]}")
-            if [ -n "$new_leader_port" ]; then
-                echo "New leader found on port $new_leader_port"
-                break # Exit the loop since a new leader has been found
-            else
-                echo "Waiting for a new leader to be elected..."
-                sleep 5
-            fi
-        done
-    else
-        echo "Error: No leader found" >&2
+    if [ -z "$leader_port" ]; then
+        echo "Exiting due to leader election failure."
         exit 1
     fi
 
+    # Kill the leader instance
+    echo "Killing the leader instance on port $leader_port..."
+    for i in "${!ports[@]}"; do
+        if [ "${ports[i]}" -eq "$leader_port" ]; then
+            kill ${pids[i]}
+            unset ports[i]
+            unset pids[i]
+            break
+        fi
+    done
+
+    sleep 5
+
+    # Restart the killed instance
+    echo "Restarting the killed instance..."
+    for instance in "${instances[@]}"; do
+        IFS=' ' read -r -a params <<< "$instance"
+        if [ "${params[4]}" -eq "$leader_port" ]; then
+            new_pid=$(start_instance "${params[0]}" "${params[1]}" "${params[2]}" "${params[3]}" "${params[5]}" "${params[6]}" "${params[7]}")
+            pids+=("$new_pid")
+            ports+=("${params[4]}")
+            readiness+=(false)
+            break
+        fi
+    done
+
+    all_ready=false
+    while [ "$all_ready" != true ]; do
+        all_ready=true
+        for i in "${!ports[@]}"; do
+            if [ "${readiness[$i]}" != true ]; then
+                response=$(check_liveness "${ports[$i]}")
+                if [ "$response" = "true" ]; then
+                    readiness[$i]=true
+                    echo "Instance on port ${ports[$i]} is ready."
+                else
+                    all_ready=false
+                fi
+            fi
+        done
+        if [ "$all_ready" != true ]; then
+            echo "Waiting for all instances to be ready..."
+            sleep 5
+        fi
+    done
+
+    echo "All instances are ready after restart. Waiting for new leader election."
+
+    # Maximum timeout duration in seconds for new leader election
+    max_timeout=60
+
+    # Capture the start time
+    start_time=$(date +%s)
+
+    # Wait until a new leader is found or timeout
+    while true; do
+        current_time=$(date +%s)
+        elapsed_time=$((current_time - start_time))
+
+        if [ $elapsed_time -ge $max_timeout ]; then
+            echo "Timeout reached without finding a new leader."
+            break # Exit the loop due to timeout
+        fi
+
+        leader_ports=($(find_leader "${ports[@]}"))
+        if check_leader_status "${leader_ports[@]}"; then
+            break
+        else
+            sleep 1
+        fi
+    done
+
     # Clean up
-    echo "Cleaning up instances..."
+    echo "Cleaning up..."
     for pid in "${pids[@]}"; do
         kill $pid
     done
@@ -230,11 +236,8 @@ run_test() {
     done
 }
 
-# Number of instances
-num_instances=3
-
 # Number of times to run the test
-n=10
+n=5
 
 # Build the project
 build_project
@@ -243,6 +246,6 @@ build_project
 for ((iteration_n=1; iteration_n<=n; iteration_n++)); do
     echo -e "\n##############################################\n"
     echo "Running test iteration $iteration_n of $n..."
-    run_test $num_instances
+    run_test
     sleep 5
 done
