bff86a3409d633c87481fa36aec3c2bbe1442fe3
Renato Dinhani <101204870+dinhani-cw@users.noreply.github.com>
2024-06-14 10:15:01 -0300

    chore: remove sqlx unused conversions (#1109)

--- src/eth/primitives/execution_result.rs
+++ src/eth/primitives/execution_result.rs
@@ -1,8 +1,3 @@
-use std::str::FromStr;
-
-use sqlx::database::HasValueRef;
-use sqlx::error::BoxDynError;
-
 /// Indicates how a transaction execution was finished.
 #[derive(Debug, strum::Display, Clone, PartialEq, Eq, fake::Dummy, derive_new::new, serde::Serialize, serde::Deserialize, strum::EnumString)]
 #[serde(rename_all = "snake_case")]
@@ -18,20 +13,4 @@ pub enum ExecutionResult {
     /// Transaction execution did not finish.
     #[strum(to_string = "halted")]
     Halted { reason: String },
-}
-
-// -----------------------------------------------------------------------------
-// Conversions: sqlx -> Self
-// -----------------------------------------------------------------------------
-impl<'r> sqlx::Decode<'r, sqlx::Postgres> for ExecutionResult {
-    fn decode(value: <sqlx::Postgres as HasValueRef<'r>>::ValueRef) -> Result<Self, BoxDynError> {
-        let value = <&str as sqlx::Decode<sqlx::Postgres>>::decode(value)?;
-        Ok(ExecutionResult::from_str(value)?)
-    }
-}
-
-impl sqlx::Type<sqlx::Postgres> for ExecutionResult {
-    fn type_info() -> <sqlx::Postgres as sqlx::Database>::TypeInfo {
-        sqlx::postgres::PgTypeInfo::with_name("TEXT")
-    }
 }
