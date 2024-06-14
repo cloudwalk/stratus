6a5a9e4a8897f4ec03a9a9c6d515e164ef5359f5
Renato Dinhani <renato.dinhani@cloudwalk.io>
2024-06-14 17:17:48 -0300

    feat: configure sentry environment

--- src/infra/sentry.rs
+++ src/infra/sentry.rs
@@ -1,15 +1,17 @@
 use sentry::ClientInitGuard;
 
+use crate::config::Environment;
 use crate::ext::not;
 use crate::infra::build_info;
 
-pub fn init_sentry(url: &str) -> anyhow::Result<ClientInitGuard> {
-    tracing::info!(%url, "creating sentry exporter");
+pub fn init_sentry(url: &str, env: Environment) -> anyhow::Result<ClientInitGuard> {
+    tracing::info!(%url, %env, "creating sentry exporter");
 
     let guard = sentry::init((
         url,
         sentry::ClientOptions {
             release: Some(build_info::service_name_with_version().into()),
+            environment: Some(env.to_string().into()),
             ..Default::default()
         },
     ));
