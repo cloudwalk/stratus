c65bb95db477d67cd643362878c0a4ecac01b54a
Renato Dinhani <renato.dinhani@cloudwalk.io>
2024-06-14 17:21:59 -0300

    logs

--- src/infra/sentry.rs
+++ src/infra/sentry.rs
@@ -5,18 +5,19 @@ use crate::ext::not;
 use crate::infra::build_info;
 
 pub fn init_sentry(url: &str, env: Environment) -> anyhow::Result<ClientInitGuard> {
-    tracing::info!(%url, %env, "creating sentry exporter");
+    let release = build_info::service_name_with_version();
+    tracing::info!(%url, %env, %release, "creating sentry exporter");
 
     let guard = sentry::init((
         url,
         sentry::ClientOptions {
-            release: Some(build_info::service_name_with_version().into()),
+            release: Some(release.clone().into()),
             environment: Some(env.to_string().into()),
             ..Default::default()
         },
     ));
     if not(guard.is_enabled()) {
-        tracing::error!(%url, "failed to create sentry exporter");
+        tracing::error!(%url, %env, %release, "failed to create sentry exporter");
     }
 
     Ok(guard)
