73fe6c2fd3ddd12de4ecc349c032497eee5c090f
Renato Dinhani <renato.dinhani@cloudwalk.io>
2024-06-14 10:09:15 -0300

    chore: remove sqlx unused conversions

--- src/eth/primitives/ecdsa_v.rs
+++ src/eth/primitives/ecdsa_v.rs
@@ -1,6 +1,4 @@
 use ethereum_types::U64;
-use sqlx::database::HasValueRef;
-use sqlx::error::BoxDynError;
 
 use crate::gen_newtype_from;
 
@@ -19,19 +17,3 @@ impl From<EcdsaV> for U64 {
 // Conversions: Self -> Other
 // -----------------------------------------------------------------------------
 gen_newtype_from!(self = EcdsaV, other = i32, [u8; 8]);
-
-// -----------------------------------------------------------------------------
-// Conversions: sqlx -> Self
-// -----------------------------------------------------------------------------
-impl<'r> sqlx::Decode<'r, sqlx::Postgres> for EcdsaV {
-    fn decode(value: <sqlx::Postgres as HasValueRef<'r>>::ValueRef) -> Result<Self, BoxDynError> {
-        let value = <[u8; 8] as sqlx::Decode<sqlx::Postgres>>::decode(value)?;
-        Ok(value.into())
-    }
-}
-
-impl sqlx::Type<sqlx::Postgres> for EcdsaV {
-    fn type_info() -> <sqlx::Postgres as sqlx::Database>::TypeInfo {
-        sqlx::postgres::PgTypeInfo::with_name("BYTEA")
-    }
-}
