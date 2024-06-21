d4bd2efea8f7f66958a2a55861091b5ec9f2fcee
gabriel-aranha-cw <gabriel.aranha@cloudwalk.io>
2024-06-21 11:57:07 -0300

    change clone

--- src/main.rs
+++ src/main.rs
@@ -22,7 +22,7 @@ async fn run(config: StratusConfig) -> anyhow::Result<()> {
     let executor = config.executor.init(Arc::clone(&storage), Arc::clone(&miner));
     let consensus = Consensus::new(
         Arc::clone(&storage),
-        config.clone().storage.perm_storage.rocks_path_prefix,
+        config.storage.perm_storage.rocks_path_prefix.clone(),
         config.clone().candidate_peers.clone(),
         None,
         config.address,
