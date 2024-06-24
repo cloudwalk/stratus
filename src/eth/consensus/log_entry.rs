6a9cf912097640fc09192a5d5a51eee05972f400
renancloudwalk <renan@cloudwalk.io>
2024-06-24 08:15:50 -0300

    fix: protobuf wire_type

--- src/eth/consensus/log_entry.rs
+++ src/eth/consensus/log_entry.rs
@@ -29,11 +29,14 @@ impl Message for LogEntryData {
         B: bytes::BufMut,
     {
         match self {
-            LogEntryData::BlockEntry(header) => header.encode_raw(buf),
-            LogEntryData::TransactionExecutionEntries(executions) =>
+            LogEntryData::BlockEntry(header) => {
+                prost::encoding::message::encode(1, header, buf);
+            }
+            LogEntryData::TransactionExecutionEntries(executions) => {
                 for execution in executions {
-                    execution.encode_raw(buf);
-                },
+                    prost::encoding::message::encode(2, execution, buf);
+                }
+            }
             LogEntryData::EmptyData => {}
         }
     }
@@ -48,22 +51,37 @@ impl Message for LogEntryData {
     where
         B: bytes::Buf,
     {
-        match self {
-            LogEntryData::BlockEntry(header) => header.merge_field(tag, wire_type, buf, ctx),
-            LogEntryData::TransactionExecutionEntries(executions) => {
-                for execution in executions {
-                    execution.merge_field(tag, wire_type, buf, ctx.clone())?;
+        match tag {
+            1 => {
+                if let LogEntryData::BlockEntry(ref mut header) = self {
+                    prost::encoding::message::merge(wire_type, header, buf, ctx)
+                } else {
+                    let mut header = BlockEntry::default();
+                    prost::encoding::message::merge(wire_type, &mut header, buf, ctx)?;
+                    *self = LogEntryData::BlockEntry(header);
+                    Ok(())
+                }
+            }
+            2 => {
+                let mut execution = TransactionExecutionEntry::default();
+                prost::encoding::message::merge(wire_type, &mut execution, buf, ctx)?;
+                if let LogEntryData::TransactionExecutionEntries(ref mut executions) = self {
+                    executions.push(execution);
+                } else {
+                    *self = LogEntryData::TransactionExecutionEntries(vec![execution]);
                 }
                 Ok(())
             }
-            LogEntryData::EmptyData => Ok(()),
+            _ => prost::encoding::skip_field(wire_type, tag, buf, ctx),
         }
     }
 
     fn encoded_len(&self) -> usize {
         match self {
-            LogEntryData::BlockEntry(header) => header.encoded_len(),
-            LogEntryData::TransactionExecutionEntries(executions) => executions.iter().map(|execution| execution.encoded_len()).sum(),
+            LogEntryData::BlockEntry(header) => prost::encoding::message::encoded_len(1, header),
+            LogEntryData::TransactionExecutionEntries(executions) => {
+                executions.iter().map(|execution| prost::encoding::message::encoded_len(2, execution)).sum()
+            }
             LogEntryData::EmptyData => 0,
         }
     }
@@ -71,9 +89,7 @@ impl Message for LogEntryData {
     fn clear(&mut self) {
         match self {
             LogEntryData::BlockEntry(header) => header.clear(),
-            LogEntryData::TransactionExecutionEntries(executions) => {
-                executions.clear();
-            }
+            LogEntryData::TransactionExecutionEntries(executions) => executions.clear(),
             LogEntryData::EmptyData => {}
         }
     }
@@ -86,7 +102,7 @@ impl Message for LogEntry {
     {
         prost::encoding::uint64::encode(1, &self.index, buf);
         prost::encoding::uint64::encode(2, &self.term, buf);
-        self.data.encode_raw(buf);
+        prost::encoding::message::encode(3, &self.data, buf); // Encode LogEntryData with the correct tag
     }
 
     fn merge_field<B>(
@@ -102,12 +118,15 @@ impl Message for LogEntry {
         match tag {
             1 => prost::encoding::uint64::merge(wire_type, &mut self.index, buf, ctx),
             2 => prost::encoding::uint64::merge(wire_type, &mut self.term, buf, ctx),
-            _ => self.data.merge_field(tag, wire_type, buf, ctx),
+            3 => prost::encoding::message::merge(wire_type, &mut self.data, buf, ctx),
+            _ => prost::encoding::skip_field(wire_type, tag, buf, ctx),
         }
     }
 
     fn encoded_len(&self) -> usize {
-        prost::encoding::uint64::encoded_len(1, &self.index) + prost::encoding::uint64::encoded_len(2, &self.term) + self.data.encoded_len()
+        prost::encoding::uint64::encoded_len(1, &self.index) +
+        prost::encoding::uint64::encoded_len(2, &self.term) +
+        prost::encoding::message::encoded_len(3, &self.data)
     }
 
     fn clear(&mut self) {
