0e245cd8b860db0f9dbd75d68cea68092ba4688c
Renato Dinhani <renato.dinhani@cloudwalk.io>
2024-06-26 17:30:56 -0300

    doc

--- src/eth/primitives/now.rs
+++ src/eth/primitives/now.rs
@@ -1,7 +1,7 @@
 use chrono::DateTime;
 use chrono::Utc;
 
-/// DateTime that automatically sets to the current time when created.
+/// DateTime that automatically sets the current time when created.
 #[derive(Debug, Clone, derive_more::Deref, serde::Serialize)]
 pub struct DateTimeNow(#[deref] DateTime<Utc>);
 
