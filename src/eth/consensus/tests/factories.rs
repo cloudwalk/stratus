9b337efd3df897d04aba34a4de4ef6a768ca62aa
renancloudwalk <renan@cloudwalk.io>
2024-06-27 08:07:51 -0300

    chore: adapt tests to the new proto definitions

--- src/eth/consensus/tests/factories.rs
+++ src/eth/consensus/tests/factories.rs
@@ -2,53 +2,43 @@ use std::net::Ipv4Addr;
 use std::net::SocketAddr;
 use std::sync::Arc;
 
-use ethereum_types::H256;
-use ethereum_types::U256;
+use ethereum_types::{H256, H160, U256};
 use rand::Rng;
 use tokio::sync::broadcast;
 use tokio::sync::Mutex;
 
-use crate::eth::consensus::append_entry::AppendBlockCommitResponse;
-use crate::eth::consensus::append_entry::AppendTransactionExecutionsResponse;
-use crate::eth::consensus::append_entry::Log;
-use crate::eth::consensus::append_entry::RequestVoteResponse;
+use crate::eth::consensus::append_entry::{
+    AppendBlockCommitResponse, AppendTransactionExecutionsResponse, Log, RequestVoteResponse,
+    TransactionExecutionEntry,
+};
+use crate::eth::consensus::{BlockEntry, Consensus, LogEntryData, Peer, PeerAddress, Role};
 use crate::eth::consensus::log_entry::LogEntry;
-use crate::eth::consensus::BlockEntry;
-use crate::eth::consensus::Consensus;
-use crate::eth::consensus::LogEntryData;
-use crate::eth::consensus::Peer;
-use crate::eth::consensus::PeerAddress;
-use crate::eth::consensus::Role;
-use crate::eth::consensus::TransactionExecutionEntry;
 use crate::eth::storage::StratusStorage;
 
-pub fn create_mock_block_entry(transaction_hashes: Vec<String>) -> BlockEntry {
+pub fn create_mock_block_entry(transaction_hashes: Vec<Vec<u8>>) -> BlockEntry {
     BlockEntry {
         number: rand::thread_rng().gen(),
-        hash: H256::random().to_string(),
-        parent_hash: H256::random().to_string(),
-        nonce: H256::random().to_string(),
-        uncle_hash: H256::random().to_string(),
-        transactions_root: H256::random().to_string(),
-        state_root: H256::random().to_string(),
-        receipts_root: H256::random().to_string(),
-        miner: H256::random().to_string(),
-        difficulty: U256::from(rand::thread_rng().gen::<u64>()).to_string(),
-        total_difficulty: U256::from(rand::thread_rng().gen::<u64>()).to_string(),
+        hash: H256::random().as_bytes().to_vec(),
+        parent_hash: H256::random().as_bytes().to_vec(),
+        uncle_hash: H256::random().as_bytes().to_vec(),
+        transactions_root: H256::random().as_bytes().to_vec(),
+        state_root: H256::random().as_bytes().to_vec(),
+        receipts_root: H256::random().as_bytes().to_vec(),
+        miner: H160::random().as_bytes().to_vec(),
+        author: H160::random().as_bytes().to_vec(),
         extra_data: vec![rand::thread_rng().gen()],
         size: rand::thread_rng().gen(),
-        gas_limit: U256::from(rand::thread_rng().gen::<u64>()).to_string(),
-        gas_used: U256::from(rand::thread_rng().gen::<u64>()).to_string(),
+        gas_limit: rand::thread_rng().gen(),
+        gas_used: rand::thread_rng().gen(),
         timestamp: rand::thread_rng().gen(),
-        bloom: H256::random().to_string(),
-        author: H256::random().to_string(),
-        transaction_hashes,
+        bloom: H256::random().as_bytes().to_vec(),
+        transaction_hashes
     }
 }
 
 pub fn create_mock_transaction_execution_entry() -> TransactionExecutionEntry {
     TransactionExecutionEntry {
-        hash: H256::random().to_string(),
+        hash: H256::random().as_bytes().to_vec(),
         nonce: rand::thread_rng().gen(),
         value: vec![rand::thread_rng().gen()],
         gas_price: vec![rand::thread_rng().gen()],
@@ -56,21 +46,18 @@ pub fn create_mock_transaction_execution_entry() -> TransactionExecutionEntry {
         v: rand::thread_rng().gen(),
         r: vec![rand::thread_rng().gen()],
         s: vec![rand::thread_rng().gen()],
-        chain_id: rand::thread_rng().gen(),
-        result: vec![rand::thread_rng().gen()],
+        chain_id: Some(rand::thread_rng().gen()),
+        result: "Success".to_string(),
         output: vec![rand::thread_rng().gen()],
-        from: H256::random().to_string(),
-        to: H256::random().to_string(),
-        block_hash: H256::random().to_string(),
+        from: H160::random().as_bytes().to_vec(),
+        to: Some(H160::random().as_bytes().to_vec()),
         block_number: rand::thread_rng().gen(),
         transaction_index: rand::thread_rng().gen(),
         logs: vec![Log {
-            address: H256::random().to_string(),
-            topics: vec![H256::random().to_string()],
+            address: H160::random().as_bytes().to_vec(),
+            topics: vec![H256::random().as_bytes().to_vec()],
             data: vec![rand::thread_rng().gen()],
             log_index: rand::thread_rng().gen(),
-            removed: rand::thread_rng().gen(),
-            transaction_log_index: rand::thread_rng().gen(),
         }],
         gas: vec![rand::thread_rng().gen()],
         receipt_cumulative_gas_used: vec![rand::thread_rng().gen()],
@@ -79,6 +66,11 @@ pub fn create_mock_transaction_execution_entry() -> TransactionExecutionEntry {
         receipt_status: rand::thread_rng().gen(),
         receipt_logs_bloom: vec![rand::thread_rng().gen()],
         receipt_effective_gas_price: vec![rand::thread_rng().gen()],
+        tx_type: Some(rand::thread_rng().gen()),
+        signer: vec![rand::thread_rng().gen()],
+        gas_limit: vec![rand::thread_rng().gen()],
+        receipt_applied: rand::thread_rng().gen(),
+        deployed_contract_address: Some(vec![rand::thread_rng().gen()]),
     }
 }
 
@@ -87,7 +79,10 @@ pub fn create_mock_log_entry_data_block() -> LogEntryData {
 }
 
 pub fn create_mock_log_entry_data_transactions() -> LogEntryData {
-    LogEntryData::TransactionExecutionEntries(vec![create_mock_transaction_execution_entry(), create_mock_transaction_execution_entry()])
+    LogEntryData::TransactionExecutionEntries(vec![
+        create_mock_transaction_execution_entry(),
+        create_mock_transaction_execution_entry(),
+    ])
 }
 
 pub fn create_mock_log_entry(index: u64, term: u64, data: LogEntryData) -> LogEntry {
--- src/eth/consensus/utils.rs
+++ src/eth/consensus/utils.rs
@@ -1,4 +1,4 @@
-fn u256_to_bytes(u: ethereum_types::U256) -> Vec<u8> {
+pub fn u256_to_bytes(u: ethereum_types::U256) -> Vec<u8> {
     let mut bytes = [0u8; 32];
     u.to_big_endian(&mut bytes);
     bytes.to_vec()
