2f5efbc8b7bb8fc70da830f6aafb859a21270633
Renato Dinhani <renato.dinhani@cloudwalk.io>
2024-06-28 01:47:55 -0300

    feat: tracing transaction identifiers in rpc root span

--- src/eth/rpc/rpc_middleware.rs
+++ src/eth/rpc/rpc_middleware.rs
@@ -17,19 +17,23 @@ use tracing::instrument::Instrumented;
 use tracing::Instrument;
 use tracing::Span;
 
+use crate::eth::primitives::Address;
 use crate::eth::primitives::Bytes;
 use crate::eth::primitives::CallInput;
+use crate::eth::primitives::Hash;
 use crate::eth::primitives::SoliditySignature;
 use crate::eth::primitives::TransactionInput;
 use crate::eth::rpc::next_rpc_param;
 use crate::eth::rpc::parse_rpc_rlp;
 use crate::eth::rpc::rpc_parser::RpcExtensionsExt;
 use crate::eth::rpc::RpcClientApp;
+use crate::ext::to_json_value;
 use crate::if_else;
 #[cfg(feature = "metrics")]
 use crate::infra::metrics;
 use crate::infra::tracing::new_cid;
 use crate::infra::tracing::SpanExt;
+use crate::infra::tracing::TracingExt;
 
 // -----------------------------------------------------------------------------
 // Active requests tracking
@@ -100,31 +104,47 @@ impl<'a> RpcServiceT<'a> for RpcMiddleware {
 
     fn call(&self, request: jsonrpsee::types::Request<'a>) -> Self::Future {
         // track request
-        let span = info_span!("rpc::request", cid = %new_cid(), rpc_client = field::Empty, rpc_id = field::Empty, rpc_method = field::Empty, rpc_function = field::Empty);
+        let span = info_span!(
+            "rpc::request",
+            cid = %new_cid(),
+            rpc_client = field::Empty,
+            rpc_id = field::Empty,
+            rpc_method = field::Empty,
+            rpc_tx_hash = field::Empty,
+            rpc_tx_from = field::Empty,
+            rpc_tx_to = field::Empty,
+            rpc_tx_function = field::Empty
+        );
         let enter = span.enter();
 
         // extract request data
         let client = request.extensions.rpc_client();
         let method = request.method_name();
-        let function = match method {
-            "eth_call" | "eth_estimateGas" => extract_call_function(request.params()),
-            "eth_sendRawTransaction" => extract_transaction_function(request.params()),
+        let tx = match method {
+            "eth_call" | "eth_estimateGas" => TxTracingIdentifiers::from_call(request.params()).ok(),
+            "eth_sendRawTransaction" => TxTracingIdentifiers::from_transaction(request.params()).ok(),
+            "eth_getTransactionByHash" | "eth_getTransactionReceipt" => TxTracingIdentifiers::from_transaction_query(request.params()).ok(),
             _ => None,
         };
+
+        // trace request
         Span::with(|s| {
             s.rec_str("rpc_id", &request.id);
             s.rec_str("rpc_client", &client);
             s.rec_str("rpc_method", &method);
-            s.rec_opt("rpc_function", &function);
+            if let Some(ref tx) = tx {
+                tx.record_span(s);
+            }
         });
-
-        // trace request
         tracing::info!(
             rpc_client = %client,
             rpc_id = %request.id,
             rpc_method = %method,
-            rpc_function = %function.clone().unwrap_or_default(),
-            rpc_params = ?request.params(),
+            rpc_params = %to_json_value(&request.params),
+            rpc_tx_hash = %tx.as_ref().and_then(|tx|tx.hash).or_empty(),
+            rpc_tx_function = %tx.as_ref().and_then(|tx|tx.function.clone()).or_empty(),
+            rpc_tx_from = %tx.as_ref().and_then(|tx|tx.from).or_empty(),
+            rpc_tx_to = %tx.as_ref().and_then(|tx|tx.to).or_empty(),
             "rpc request"
         );
 
@@ -132,7 +152,7 @@ impl<'a> RpcServiceT<'a> for RpcMiddleware {
         #[cfg(feature = "metrics")]
         {
             active_requests::COUNTERS.inc(&client, method);
-            metrics::inc_rpc_requests_started(&client, method, function.clone());
+            metrics::inc_rpc_requests_started(&client, method, tx.as_ref().and_then(|tx| tx.function.clone()));
         }
 
         drop(enter);
@@ -141,7 +161,7 @@ impl<'a> RpcServiceT<'a> for RpcMiddleware {
                 client,
                 id: request.id.to_string(),
                 method: method.to_string(),
-                function,
+                tx,
             },
             start: Instant::now(),
             future_response: self.service.call(request),
@@ -168,24 +188,27 @@ impl<'a> Future for RpcResponse<'a> {
 
     fn poll(self: std::pin::Pin<&mut Self>, cx: &mut std::task::Context<'_>) -> std::task::Poll<Self::Output> {
         // poll future
-        let proj = self.project();
-        let response = proj.future_response.poll(cx);
+        let resp = self.project();
+        let response = resp.future_response.poll(cx);
 
         // when ready, track response
         if let Poll::Ready(response) = &response {
-            let elapsed = proj.start.elapsed();
+            let elapsed = resp.start.elapsed();
 
             // trace response
             let response_success = response.is_success();
             let response_result = response.as_result();
             tracing::info!(
-                rpc_client = %proj.identifiers.client,
-                rpc_id = %proj.identifiers.id,
-                rpc_method = %proj.identifiers.method,
-                rpc_function = %proj.identifiers.function.clone().unwrap_or_default(),
-                rpc_duration_us = %elapsed.as_micros(),
-                rpc_success = %response_success,
+                rpc_client = %resp.identifiers.client,
+                rpc_id = %resp.identifiers.id,
+                rpc_method = %resp.identifiers.method,
+                rpc_tx_hash = %resp.identifiers.tx.as_ref().and_then(|tx|tx.hash).or_empty(),
+                rpc_tx_function = %resp.identifiers.tx.as_ref().and_then(|tx|tx.function.clone()).or_empty(),
+                rpc_tx_from = %resp.identifiers.tx.as_ref().and_then(|tx|tx.from).or_empty(),
+                rpc_tx_to = %resp.identifiers.tx.as_ref().and_then(|tx|tx.to).or_empty(),
                 rpc_result = %response_result,
+                rpc_success = %response_success,
+                rpc_duration_us = %elapsed.as_micros(),
                 "rpc response"
             );
 
@@ -199,9 +222,9 @@ impl<'a> Future for RpcResponse<'a> {
 
                 metrics::inc_rpc_requests_finished(
                     elapsed,
-                    &proj.identifiers.client,
-                    proj.identifiers.method.clone(),
-                    proj.identifiers.function.clone(),
+                    &resp.identifiers.client,
+                    resp.identifiers.method.clone(),
+                    resp.identifiers.tx.as_ref().and_then(|tx| tx.function.clone()),
                     rpc_result,
                     response.is_success(),
                 );
@@ -216,7 +239,7 @@ struct RpcResponseIdentifiers {
     client: RpcClientApp,
     id: String,
     method: String,
-    function: Option<SoliditySignature>,
+    tx: Option<TxTracingIdentifiers>,
 }
 
 impl Drop for RpcResponseIdentifiers {
@@ -232,13 +255,57 @@ impl Drop for RpcResponseIdentifiers {
 // Helpers
 // -----------------------------------------------------------------------------
 
-fn extract_call_function(params: Params) -> Option<SoliditySignature> {
-    let (_, call) = next_rpc_param::<CallInput>(params.sequence()).ok()?;
-    call.extract_function()
+struct TxTracingIdentifiers {
+    pub hash: Option<Hash>,
+    pub function: Option<SoliditySignature>,
+    pub from: Option<Address>,
+    pub to: Option<Address>,
 }
 
-fn extract_transaction_function(params: Params) -> Option<SoliditySignature> {
-    let (_, data) = next_rpc_param::<Bytes>(params.sequence()).ok()?;
-    let transaction = parse_rpc_rlp::<TransactionInput>(&data).ok()?;
-    transaction.extract_function()
+impl TxTracingIdentifiers {
+    fn from_transaction(params: Params) -> anyhow::Result<Self> {
+        let (_, data) = next_rpc_param::<Bytes>(params.sequence())?;
+        let tx = parse_rpc_rlp::<TransactionInput>(&data)?;
+        Ok(Self {
+            hash: Some(tx.hash),
+            function: tx.extract_function(),
+            from: Some(tx.signer),
+            to: tx.to,
+        })
+    }
+
+    fn from_call(params: Params) -> anyhow::Result<Self> {
+        let (_, call) = next_rpc_param::<CallInput>(params.sequence())?;
+        Ok(Self {
+            hash: None,
+            function: call.extract_function(),
+            from: call.from,
+            to: call.to,
+        })
+    }
+
+    fn from_transaction_query(params: Params) -> anyhow::Result<Self> {
+        let (_, hash) = next_rpc_param::<Hash>(params.sequence())?;
+        Ok(Self {
+            hash: Some(hash),
+            function: None,
+            from: None,
+            to: None,
+        })
+    }
+
+    pub fn record_span(&self, span: Span) {
+        if let Some(tx_hash) = self.hash {
+            span.rec_str("rpc_tx_hash", &tx_hash);
+        }
+        if let Some(ref tx_function) = self.function {
+            span.rec_str("rpc_tx_function", &tx_function);
+        }
+        if let Some(tx_from) = self.from {
+            span.rec_str("rpc_tx_from", &tx_from);
+        }
+        if let Some(tx_to) = self.to {
+            span.rec_str("rpc_tx_to", &tx_to);
+        }
+    }
 }
