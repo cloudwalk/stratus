5ebf9bf91a21250119b3baa2682285cc675eb049
João Marcos <164224824+marcospb19-cw@users.noreply.github.com>
2024-07-09 15:54:16 -0300

    chores: rocksdb: use size units (#1379)

--- src/eth/storage/rocks/rocks_config.rs
+++ src/eth/storage/rocks/rocks_config.rs
@@ -2,6 +2,13 @@ use rocksdb::BlockBasedOptions;
 use rocksdb::Cache;
 use rocksdb::Options;
 
+const GIGABYTE: usize = 1024 * 1024 * 1024;
+const MEGABYTE: usize = 1024 * 1024;
+const KILOBYTE: usize = 1024;
+
+const GIGABYTE_U64: u64 = 1024 * 1024 * 1024;
+const MEGABYTE_U64: u64 = 1024 * 1024;
+
 #[derive(Debug, Clone, Copy)]
 pub enum CacheSetting {
     /// Enabled cache with the given size in bytes
@@ -44,7 +51,7 @@ impl DbConfig {
                 opts.set_compaction_style(rocksdb::DBCompactionStyle::Level);
 
                 // Configure the size of SST files at each level
-                opts.set_target_file_size_base(512 * 1024 * 1024);
+                opts.set_target_file_size_base(512 * MEGABYTE_U64);
 
                 // Increase the file size multiplier to expand file size at upper levels
                 opts.set_target_file_size_multiplier(2); // Each level grows in file size quicker
@@ -57,20 +64,20 @@ impl DbConfig {
                 opts.set_level_zero_stop_writes_trigger(20);
 
                 // Increase the max bytes for L1 to allow more data before triggering compaction
-                opts.set_max_bytes_for_level_base(2048 * 1024 * 1024);
+                opts.set_max_bytes_for_level_base(2 * GIGABYTE_U64);
 
                 // Increase the level multiplier to aggressively increase space at each level
                 opts.set_max_bytes_for_level_multiplier(8.0); // Exponential growth of levels is more pronounced
 
                 // Configure block size to optimize for larger blocks, improving sequential read performance
-                block_based_options.set_block_size(128 * 1024); // 128KB blocks
+                block_based_options.set_block_size(128 * KILOBYTE);
 
                 // Increase the number of write buffers to delay flushing, optimizing CPU usage for compaction
                 opts.set_max_write_buffer_number(5);
-                opts.set_write_buffer_size(128 * 1024 * 1024); // 128MB per write buffer
+                opts.set_write_buffer_size(128 * MEGABYTE); // 128MB per write buffer
 
                 // Keep a higher number of open files to accommodate more files being produced by aggressive compaction
-                opts.set_max_open_files(20000);
+                opts.set_max_open_files(20_000);
 
                 // Apply more aggressive compression settings, if I/O and CPU permit
                 opts.set_compression_per_level(&[
@@ -83,7 +90,7 @@ impl DbConfig {
                 opts.set_compaction_style(rocksdb::DBCompactionStyle::Level);
 
                 // Increase initial SST file sizes to reduce the frequency of writes to disk
-                opts.set_target_file_size_base(512 * 1024 * 1024); // Starting at 512MB for L1
+                opts.set_target_file_size_base(512 * MEGABYTE_U64); // Starting at 512MB for L1
 
                 // Minimize the file size multiplier to control the growth of file sizes at upper levels
                 opts.set_target_file_size_multiplier(1); // Minimal increase in file size at upper levels
@@ -93,20 +100,20 @@ impl DbConfig {
                 opts.set_level_zero_stop_writes_trigger(200); // Stop writes at 200 L0 files
 
                 // Expand the maximum bytes for base level to further delay the need for compaction-related I/O
-                opts.set_max_bytes_for_level_base(2048 * 1024 * 1024);
+                opts.set_max_bytes_for_level_base(2048 * MEGABYTE_U64);
 
                 // Use a higher level multiplier to increase space exponentially at higher levels
                 opts.set_max_bytes_for_level_multiplier(10.0);
 
                 // Opt for larger block sizes to decrease the number of read and write operations to disk
-                block_based_options.set_block_size(512 * 1024); // 512KB blocks
+                block_based_options.set_block_size(512 * KILOBYTE); // 512KB blocks
 
                 // Maximize the use of write buffers to extend the time data stays in memory before flushing
                 opts.set_max_write_buffer_number(16);
-                opts.set_write_buffer_size(1024 * 1024 * 1024); // 1GB per write buffer
+                opts.set_write_buffer_size(GIGABYTE); // 1GB per write buffer
 
                 // Allow a very high number of open files to minimize the overhead of opening and closing files
-                opts.set_max_open_files(20000);
+                opts.set_max_open_files(20_000);
 
                 // Choose compression that balances CPU use and effective storage reduction
                 opts.set_compression_per_level(&[rocksdb::DBCompressionType::Lz4, rocksdb::DBCompressionType::Zstd]);
