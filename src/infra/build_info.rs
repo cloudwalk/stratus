adf4bad424560f6ce660cf0bddb58aaddf98ece1
renancloudwalk <renan@cloudwalk.io>
2024-07-11 15:06:58 -0300

    fix: add missing use

--- src/infra/build_info.rs
+++ src/infra/build_info.rs
@@ -1,6 +1,7 @@
 use serde_json::json;
 
 use crate::eth::rpc::RpcContext;
+use crate::eth::Consensus;
 use crate::ext::JsonValue;
 
 // -----------------------------------------------------------------------------
