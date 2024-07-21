1a61c03e05835065ef6210339a97d9ae9c814470
Renato Dinhani <renato.dinhani@cloudwalk.io>
2024-07-21 16:13:03 -0300

    enha: change lock order

--- src/eth/executor/executor.rs
+++ src/eth/executor/executor.rs
@@ -351,17 +351,6 @@ impl Executor {
             s.rec_str("tx_nonce", &tx_input.nonce);
         });
 
-        // WORKAROUND: prevents interval miner mining blocks while a transaction is being executed.
-        let _miner_lock = if self.miner.mode.is_interval() {
-            Some(self.miner.locks.mine_and_commit.lock().unwrap_or_else(|poison| {
-                tracing::warn!("miner mine_and_commit lock was poisoned");
-                self.locks.serial.clear_poison();
-                poison.into_inner()
-            }))
-        } else {
-            None
-        };
-
         // execute according to the strategy
         const INFINITE_ATTEMPTS: usize = usize::MAX;
 
@@ -376,8 +365,21 @@ impl Executor {
                     self.locks.serial.clear_poison();
                     poison.into_inner()
                 });
+
+                // WORKAROUND: prevents interval miner mining blocks while a transaction is being executed.
+                let _miner_lock = if self.miner.mode.is_interval() {
+                    Some(self.miner.locks.mine_and_commit.lock().unwrap_or_else(|poison| {
+                        tracing::warn!("miner mine_and_commit lock was poisoned");
+                        self.locks.serial.clear_poison();
+                        poison.into_inner()
+                    }))
+                } else {
+                    None
+                };
+
                 self.execute_local_transaction_attempts(tx_input, EvmRoute::Serial, INFINITE_ATTEMPTS)
             }
+
             // Executes transactions in parallel mode:
             // * Conflict detection prevents data corruption.
             ExecutorStrategy::Paralell => {
