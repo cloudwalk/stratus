a41b0a6e8edbce38f5179358c18952f5b561dcdb
Daniel Freire <daniel.freire@cloudwalk.io>
2024-07-24 17:48:17 -0300

    serialize block number to be (thanks MPB)

--- src/eth/storage/rocks/types/block_number.rs
+++ src/eth/storage/rocks/types/block_number.rs
@@ -3,7 +3,7 @@ use std::fmt::Debug;
 use crate::eth::primitives::BlockNumber;
 use crate::gen_newtype_from;
 
-#[derive(Debug, derive_more::Display, Clone, Copy, Default, Eq, PartialEq, PartialOrd, Ord, Hash, serde::Serialize, serde::Deserialize)]
+#[derive(Debug, derive_more::Display, Clone, Copy, Default, Eq, PartialEq, PartialOrd, Ord, Hash)]
 pub struct BlockNumberRocksdb(pub u64);
 
 gen_newtype_from!(self = BlockNumberRocksdb, other = u8, u16, u32, u64);
@@ -23,5 +23,17 @@ impl From<BlockNumberRocksdb> for BlockNumber {
 impl From<BlockNumberRocksdb> for u64 {
     fn from(value: BlockNumberRocksdb) -> Self {
         value.0
+    }
+}
+
+impl serde::Serialize for BlockNumberRocksdb {
+    fn serialize<S: serde::Serializer>(&self, serializer: S) -> Result<S::Ok, S::Error> {
+        self.0.to_be().serialize(serializer)
+    }
+}
+
+impl<'de> serde::Deserialize<'de> for BlockNumberRocksdb {
+    fn deserialize<D: serde::Deserializer<'de>>(deserializer: D) -> Result<Self, D::Error> {
+        u64::deserialize(deserializer).map(|v| Self(u64::from_be(v)))
     }
 }
