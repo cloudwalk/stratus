bc75bebda76ccbfdf48bc6cbc9d61731c4ad5c08
Maycon Amaro <maycon.amaro@cloudwalk.io>
2024-07-26 14:22:27 -0300

    format miner.rs

--- src/eth/miner/miner.rs
+++ src/eth/miner/miner.rs
@@ -153,9 +153,7 @@ impl Miner {
         let mined_txs = mine_external_transactions(block.number, external_txs)?;
         let block = block_from_external(external_block, mined_txs);
 
-        block.inspect(|block| {
-            Span::with(|s| s.rec_str("block_number", &block.number()))
-        })
+        block.inspect(|block| Span::with(|s| s.rec_str("block_number", &block.number())))
     }
 
     /// Same as [`Self::mine_external_mixed`], but automatically commits the block instead of returning it.
@@ -240,9 +238,7 @@ impl Miner {
             None => Ok(Block::new_at_now(block.number)),
         };
 
-        block.inspect(|block| {
-            Span::with(|s| s.rec_str("block_number", &block.number()))
-        })
+        block.inspect(|block| Span::with(|s| s.rec_str("block_number", &block.number())))
     }
 
     /// Persists a mined block to permanent storage and prepares new block.
