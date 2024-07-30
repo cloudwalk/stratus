4c6b78173e8bb1bc3c281cc9e8b0b9b79e5064b9
Renato Dinhani <101204870+dinhani-cw@users.noreply.github.com>
2024-07-30 15:15:38 -0300

    enha: more serde tests (#1568)

--- src/eth/primitives/ecdsa_rs.rs
+++ src/eth/primitives/ecdsa_rs.rs
@@ -1,10 +1,12 @@
+use display_json::DebugAsJson;
 use ethereum_types::U256;
+use fake::Dummy;
+use fake::Faker;
 
 use crate::gen_newtype_from;
 
-// Type representing `r` and `s` variables
-// from the ECDSA signature
-#[derive(Clone, Copy)]
+// Type representing `r` and `s` variables from the ECDSA signature.
+#[derive(DebugAsJson, Clone, Copy, PartialEq, Eq, serde::Serialize, serde::Deserialize)]
 pub struct EcdsaRs(U256);
 
 impl From<EcdsaRs> for U256 {
@@ -13,7 +15,13 @@ impl From<EcdsaRs> for U256 {
     }
 }
 
+impl Dummy<Faker> for EcdsaRs {
+    fn dummy_with_rng<R: rand::prelude::Rng + ?Sized>(_: &Faker, rng: &mut R) -> Self {
+        Self(U256([rng.next_u64(), rng.next_u64(), rng.next_u64(), rng.next_u64()]))
+    }
+}
+
 // -----------------------------------------------------------------------------
 // Conversions: Self -> Other
 // -----------------------------------------------------------------------------
-gen_newtype_from!(self = EcdsaRs, other = i64, [u8; 32]);
+gen_newtype_from!(self = EcdsaRs, other = u8, u16, u32, u64, u128, U256, i8, i16, i32, i64, i128, [u8; 32]);
