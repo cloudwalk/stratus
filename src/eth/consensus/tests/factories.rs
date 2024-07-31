02e731dce7d2608927134cb6e9004dfbffe4974b
renancloudwalk <renan@cloudwalk.io>
2024-07-31 15:27:01 -0300

    lint

--- src/eth/consensus/tests/factories.rs
+++ src/eth/consensus/tests/factories.rs
@@ -119,10 +119,7 @@ pub fn create_mock_consensus() -> Arc<Consensus> {
     let tmpdir_log_entries_path = tmpdir_log_entries.path().to_str().map(|s| s.to_owned());
     let storage = Arc::new(storage);
 
-    let miner = Miner::new(
-        Arc::clone(&storage),
-        crate::eth::miner::MinerMode::External,
-    );
+    let miner = Miner::new(Arc::clone(&storage), crate::eth::miner::MinerMode::External);
 
     Consensus::new(
         Arc::clone(&storage),
