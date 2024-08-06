5a0acb1af3137f81f58fc95b54e9baadb2d6e905
Renato Dinhani <renato.dinhani@cloudwalk.io>
2024-08-06 11:25:04 -0300

    feat: architecture diagram using structurizr

--- static/diagrams/stratus-components.main.kts
+++ static/diagrams/stratus-components.main.kts
@@ -1,4 +1,11 @@
-// kotlin stratus-components.main.kts
+// Dependencies:
+// * Kotlin and JVM for running this script.
+// * mermaid-cli for Mermaid diagrams.
+// * Graphviz for DOT diagrams.
+// * PlantUML for PlantUML diagrams.
+//
+// Usage:
+// kotlin stratus-components.main.kts | bash
 
 // -----------------------------------------------------------------------------
 // Imports
@@ -8,7 +15,9 @@
 
 import com.structurizr.*
 import com.structurizr.export.*
+import com.structurizr.export.dot.*
 import com.structurizr.export.mermaid.*
+import com.structurizr.export.plantuml.*
 import com.structurizr.model.*
 import com.structurizr.view.*
 import java.io.File
@@ -18,23 +27,76 @@ import java.io.File
 // -----------------------------------------------------------------------------
 val workspace = Workspace("Stratus", null)
 val views = workspace.views
+val styles = views.configuration.styles
 val model = workspace.model
-val stratus = model.addSoftwareSystem("Stratus")
 
 // -----------------------------------------------------------------------------
 // Components
 // -----------------------------------------------------------------------------
-val core = stratus.addContainer("Core")
-val storage = stratus.addContainer("Storage")
+val stratus = model.addSoftwareSystem("Stratus")
+val stratusInternals = stratus.addContainer("Internals")
+
+val importer    = stratusInternals.addComponent("Importer Online")
+val rpcServer   = stratusInternals.addComponent("RPC: Server")
+val rpcSubs     = stratusInternals.addComponent("RPC: Subscriptions")
+val executor    = stratusInternals.addComponent("Core: Executor")
+val miner       = stratusInternals.addComponent("Core: Miner")
+val storage     = stratusInternals.addComponent("Storage")
+val storagePerm = stratusInternals.addComponent("Storage: Permanent").also  { it.technology = "RocksDB";  }
+val storageTemp = stratusInternals.addComponent("Storage: Temporary").also { it.technology = "In-Memory"}
+
+// -----------------------------------------------------------------------------
+// Relationships
+// -----------------------------------------------------------------------------
+importer.uses(executor, "execute")
+
+rpcServer.uses(rpcSubs, "manage")
+rpcServer.uses(storage, "read")
+rpcServer.uses(executor, "execute")
+
+rpcSubs.uses(miner, "subscribe")
+
+executor.uses(storage, "read")
+executor.uses(miner, "writes")
+
+miner.uses(storage, "write")
+
+storage.uses(storageTemp, "read/write")
+storage.uses(storagePerm, "read/write")
 
 // -----------------------------------------------------------------------------
 // Views
 // -----------------------------------------------------------------------------
-val stratusView = views.createSystemContextView(stratus, "Internal components", "")
-stratusView.addAllElements()
+val componentsView = views.createComponentView(stratusInternals, "", "")
+componentsView.addAllElements()
 
 // -----------------------------------------------------------------------------
-// Exporter
+// Exporters
 // -----------------------------------------------------------------------------
-val diagram = MermaidDiagramExporter().export(stratusView)
-File("stratus-components.md").writeText("```mermaid\n${diagram.definition}\n```")
+fun export(exporter: AbstractDiagramExporter, view: ComponentView): File {
+    if (exporter is MermaidDiagramExporter) {
+        view.enableAutomaticLayout(AutomaticLayout.RankDirection.LeftRight) // mermaid is bugged
+    } else {
+        view.enableAutomaticLayout(AutomaticLayout.RankDirection.TopBottom)
+    }
+    val diagram = exporter.export(componentsView)
+    return createTempFile(prefix = "stratus-", suffix = ".diagram").also { it .writeText(diagram.definition) }
+}
+
+val mermaidFile = export(MermaidDiagramExporter(), componentsView)
+val dotFile = export(DOTExporter(), componentsView)
+val plantUmlFile = export(C4PlantUMLExporter(), componentsView)
+
+val renderCommands = listOf(
+    "echo Rendering Mermaid",
+    "mmdc -i $mermaidFile -o ./rendered-stratus-components-mermaid.svg",
+    "echo Rendering Graphviz",
+    "dot  -Tsvg $dotFile -o ./rendered-stratus-components-graphviz.svg",
+    "echo Rendering PlantUML",
+    "java -jar /usr/local/bin/plantuml.jar -failfast -tsvg -o $(pwd)/plantuml $plantUmlFile",
+    "mv ./plantuml/*.svg ./rendered-stratus-components-plantuml.svg",
+    "rm -rf plantuml"
+
+)
+println(renderCommands.joinToString("\n"))
+
