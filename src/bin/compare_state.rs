a3f54cabeb2f0b91d8ebbc9f6907d6076f842b4c
Daniel Freire <daniel.freire@cloudwalk.io>
2024-09-06 19:05:16 -0300

    point lookupts

--- src/bin/compare_state.rs
+++ src/bin/compare_state.rs
@@ -15,7 +15,6 @@ use stratus::eth::storage::rocks::rocks_cf::RocksCfRef;
 use stratus::eth::storage::rocks::types::AddressRocksdb;
 use stratus::eth::storage::rocks::types::BlockNumberRocksdb;
 use stratus::eth::storage::rocks::types::SlotIndexRocksdb;
-use stratus::eth::storage::rocks::types::SlotValueRocksdb;
 
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
@@ -58,94 +57,44 @@ fn main() -> anyhow::Result<()> {
 
     let mut differences = 0;
 
-    let mut iter1 = cf1.iter_start();
-    let mut iter2 = cf2.iter_start();
+    let old_db_iter = cf2.iter_start();
 
     let progress_bar = indicatif::ProgressBar::new(num_keys);
     let max_block: BlockNumberRocksdb = args.max_block.into();
     println!("Starting state comparison");
-    while let (Some(result1), Some(result2)) = (iter1.next(), iter2.next()) {
-        let Ok(((address1, slot_index1, block_number1), value1)) = result1.context("Error iterating over db1") else {
-            continue;
-        };
-        let Ok(((address2, slot_index2, block_number2), value2)) = result2.context("Error iterating over db2") else {
+    for result in progress_bar.wrap_iter(old_db_iter) {
+        let Ok(((address, slot_index, block_number), value_old)) = result.context("Error iterating over db2") else {
             continue;
         };
 
-        if block_number1 > max_block && BlockNumberRocksdb::from(block_number2.0.as_u64()) > max_block {
-            progress_bar.inc(1);
+        if BlockNumberRocksdb::from(block_number.0.as_u64()) > max_block {
             continue;
         }
 
-        // If block_numberA > max_block but block_numberB < max_block then cfB does not have the entry for that (and presumably future) blocks
-        // Therefore cfB is already in another slot and cfA need to iterate from that slot.
-        if block_number1 > max_block {
-            iter1 = cf1.iter_from(
-                (
-                    Address::from(address2.0).into(),
-                    SlotIndex::from(slot_index2.0).into(),
-                    block_number2.0.as_u64().into(),
-                ),
-                rocksdb::Direction::Forward,
-            )?;
-            progress_bar.inc(1);
-            continue;
-        }
+        let key = (
+            Address::from(address.0).into(),
+            SlotIndex::from(slot_index.0).into(),
+            block_number.0.as_u64().into(),
+        );
 
-        if BlockNumberRocksdb::from(block_number2.0.as_u64()) > max_block {
-            iter2 = cf2.iter_from(
-                (
-                    AddressRocksdbOld(address1.0.into()),
-                    SlotIndexRocksdbOld(U256(slot_index1.0)),
-                    BlockNumberRocksdbOld(block_number1.0.into()),
-                ),
-                rocksdb::Direction::Forward,
-            )?;
-            progress_bar.inc(1);
-            continue;
+        match cf1.get(&key)? {
+            Some(value_new) => {
+                if value_new != SlotValue::from(value_old.0).into() {
+                    println!(
+                        "Difference found at address: {:?}, slot: {:?}, block: {:?}",
+                        address, slot_index, block_number
+                    );
+                    println!("  Value old: {:?}", value_old);
+                    println!("  Value new: {:?}", value_new);
+                    differences += 1;
+                }
+            }
+            None => {
+                println!("Entry missing in new DB:");
+                println!("  Old DB: address: {:?}, slot: {:?}, block: {:?}", address, slot_index, block_number);
+                differences += 1;
+            }
         }
-
-        if (address1, slot_index1, block_number1)
-            != (
-                Address::from(address2.0).into(),
-                SlotIndex::from(slot_index2.0).into(),
-                block_number2.0.as_u64().into(),
-            )
-        {
-            println!("Mismatched entries:");
-            println!("  DB1: address: {:?}, slot: {:?}, block: {:?}", address1, slot_index1, block_number1);
-            println!("  DB2: address: {:?}, slot: {:?}, block: {:?}", address2, slot_index2, block_number2);
-            differences += 1;
-        } else if value1 != SlotValue::from(value2.0).into() {
-            println!(
-                "Difference found at address: {:?}, slot: {:?}, block: {:?}",
-                address1, slot_index1, block_number1
-            );
-            println!("  Value 1: {:?}", value1);
-            println!("  Value 2: {:?}", value2);
-            differences += 1;
-        }
-
-        progress_bar.inc(1);
-    }
-
-    // Check for any remaining entries in either database
-    for result in iter1 {
-        let ((address, slot_index, block_number), _) = result.context("Error iterating over remaining db1 entries")?;
-        if block_number <= max_block {
-            println!("Extra entry in db1: address: {:?}, slot: {:?}, block: {:?}", address, slot_index, block_number);
-            differences += 1;
-        }
-        progress_bar.inc(1);
-    }
-
-    for result in iter2 {
-        let ((address, slot_index, block_number), _) = result.context("Error iterating over remaining db2 entries")?;
-        if BlockNumberRocksdb::from(block_number.0.as_u64()) <= max_block {
-            println!("Extra entry in db2: address: {:?}, slot: {:?}, block: {:?}", address, slot_index, block_number);
-            differences += 1;
-        }
-        progress_bar.inc(1);
     }
 
     progress_bar.finish();
