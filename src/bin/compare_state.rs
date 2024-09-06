f02f8ab99e097f797c6c3f4d16f3ecfca52cdb9d
Daniel Freire <daniel.freire@cloudwalk.io>
2024-09-06 11:22:05 -0300

    iterate over both databases at the same time

--- src/bin/compare_state.rs
+++ src/bin/compare_state.rs
@@ -2,7 +2,6 @@ use anyhow::Context;
 use clap::Parser;
 use rocksdb::{DB, Options};
 use stratus::eth::storage::rocks::rocks_cf::RocksCfRef;
-use stratus::eth::storage::rocks::rocks_state::RocksStorageState;
 use stratus::eth::primitives::{Address, BlockNumber, SlotIndex, SlotValue};
 
 #[derive(Parser, Debug)]
@@ -29,20 +28,51 @@ fn main() -> anyhow::Result<()> {
 
     let mut differences = 0;
 
-    for result in cf1.iter_start() {
-        let ((address, slot_index, block_number), value1) = result.context("Error iterating over db1")?;
+    let mut iter1 = cf1.iter_start();
+    let mut iter2 = cf2.iter_start();
 
-        if block_number > args.max_block {
+    while let (Some(result1), Some(result2)) = (iter1.next(), iter2.next()) {
+        let ((address1, slot_index1, block_number1), value1) = result1.context("Error iterating over db1")?;
+        let ((address2, slot_index2, block_number2), value2) = result2.context("Error iterating over db2")?;
+
+        if block_number1 > args.max_block && block_number2 > args.max_block {
             continue;
         }
 
-        if let Some(value2) = cf2.get(&(address, slot_index, block_number))? {
-            if value1 != value2 {
-                println!("Difference found at address: {:?}, slot: {:?}, block: {:?}", address, slot_index, block_number);
-                differences += 1;
-            }
-        } else {
-            println!("Entry missing in db2 for address: {:?}, slot: {:?}, block: {:?}", address, slot_index, block_number);
+        if block_number1 > args.max_block {
+            iter1 = cf1.iter_from((address2, slot_index2, block_number2), rocksdb::Direction::Forward)?;
+            continue;
+        }
+
+        if block_number2 > args.max_block {
+            iter2 = cf2.iter_from((address1, slot_index1, block_number1), rocksdb::Direction::Forward)?;
+            continue;
+        }
+
+        if (address1, slot_index1, block_number1) != (address2, slot_index2, block_number2) {
+            println!("Mismatch in entry order:");
+            println!("  DB1: address: {:?}, slot: {:?}, block: {:?}", address1, slot_index1, block_number1);
+            println!("  DB2: address: {:?}, slot: {:?}, block: {:?}", address2, slot_index2, block_number2);
+            differences += 1;
+        } else if value1 != value2 {
+            println!("Difference found at address: {:?}, slot: {:?}, block: {:?}", address1, slot_index1, block_number1);
+            differences += 1;
+        }
+    }
+
+    // Check for any remaining entries in either database
+    while let Some(result) = iter1.next() {
+        let ((address, slot_index, block_number), _) = result.context("Error iterating over remaining db1 entries")?;
+        if block_number <= args.max_block {
+            println!("Extra entry in db1: address: {:?}, slot: {:?}, block: {:?}", address, slot_index, block_number);
+            differences += 1;
+        }
+    }
+
+    while let Some(result) = iter2.next() {
+        let ((address, slot_index, block_number), _) = result.context("Error iterating over remaining db2 entries")?;
+        if block_number <= args.max_block {
+            println!("Extra entry in db2: address: {:?}, slot: {:?}, block: {:?}", address, slot_index, block_number);
             differences += 1;
         }
     }
