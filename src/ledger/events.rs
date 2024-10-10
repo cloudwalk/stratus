a436e7428e65bdb752a08642a467ec942712dfa3
Renato Dinhani <renato.dinhani@cloudwalk.io>
2024-10-10 10:02:09 -0300

    enha: direction field, transaction_date renamed, docs updated

--- src/ledger/events.rs
+++ src/ledger/events.rs
@@ -9,10 +9,12 @@ use crate::eth::primitives::Hash;
 
 /// Represents token transfers (debits and credits) associated with a specific Ethereum account within a single transaction.
 ///
-/// The `account_address` field identifies the main account involved in these transfers.
+/// The `primary_account_address` field identifies the primary account involved in these transfers.
 ///
-/// An event will be generated for each account involved in a transaction, meaning if a transaction is not the main account in this event,
-/// in another event it will be and credit and debit parties will be adjusted accordingly.
+/// An event will be generated for each account involved in a transaction, meaning if a transaction is not the primary in this event,
+/// in another event it will treated as the primary and credit and debit operations adjusted accordingly.
+///
+/// A single event can contain multiple token transfers (e.g., a customer is debited for a card payment but receives a credit as cashback)
 pub struct AccountTransfersEvent {
     /// ID of the event publication.
     ///
@@ -26,7 +28,7 @@ pub struct AccountTransfersEvent {
     /// Format: ISO 8601
     pub publication_datetime: DateTime<Utc>,
 
-    /// Idempotency key of the event payload (TBD: `account_address` + `transaction_hash`).
+    /// Idempotency key of the event payload (TBD: `primary_account_address` + `transaction_hash`).
     ///
     /// It is unique for each distinct event, but consistent across retries or republications of the same event payload.
     ///
@@ -36,12 +38,17 @@ pub struct AccountTransfersEvent {
     /// Address of the account that is part of all transfers.
     ///
     /// Format: Prefixed account address - 20 bytes - 0x1234567890123456789012345678901234567890
-    pub account_address: Address,
+    pub primary_account_address: Address,
 
     /// Hash of the Ethereum transaction that originated transfers.
     ///
     /// Format: Prefixed transaction hash - 32 bytes - 0x1234567890123456789012345678901234567890123456789012345678901234
     pub transaction_hash: Hash,
+
+    /// Datetime of the Ethereum transaction that originated transfers.
+    ///
+    /// Format: ISO 8601
+    pub transaction_datetime: DateTime<Utc>,
 
     /// Address of the contract that originated transfers.
     ///
@@ -55,23 +62,18 @@ pub struct AccountTransfersEvent {
 
     /// Number of the block that originated transfers.
     ///
-    /// Format: Integer
+    /// Format: Integer (base 10)
     pub block_number: BlockNumber,
-
-    /// Datetime of the block that originated account transfers.
-    ///
-    /// Format: ISO 8601
-    pub block_datetime: DateTime<Utc>,
 
     /// List of transfers the related `account_address` is part of.
     pub transfers: Vec<AccountTransfer>,
 }
 
-/// Represents a token transfer between a debit party and a credit party.
+/// Represents a token transfer between a debit party and a credit party that happened inside a transaction.
 pub struct AccountTransfer {
     /// Address of the token contract that executed the transfer between `debit_party_address` and `credit_party_address`.
     ///
-    /// It may differ from the main `contract_address` because any contract can execute transfers in token contracts.
+    /// It may differ from the `contract_address` because any contract can execute transfers in token contracts.
     ///
     /// Format: Prefixed account address - 20 bytes - 0x1234567890123456789012345678901234567890
     pub token_address: Address,
@@ -88,8 +90,20 @@ pub struct AccountTransfer {
 
     /// Amount transferred from debit party to credit party.
     ///
-    /// Format: TBD
-    /// * Prefixed hexadecimal amount - 32 bytes - 0x123450x1234567890123456789012345678901234567890678901234567890
-    /// * Decimal amount without prefix or padding - Integer - 1234
+    /// Format: Integer (base 10)
     pub amount: U256,
+
+    /// Direction of the transfer relative to the primary account (credit or debit).
+    ///
+    /// Format: [debit, credit]
+    pub direction: AccountTransferDirection,
+}
+
+/// Direction of a transfer relative to the primary account.
+pub enum AccountTransferDirection {
+    /// `primary_account_address` is being credited.
+    Credit,
+
+    /// `primary_account_address` is being debited.
+    Debit,
 }
