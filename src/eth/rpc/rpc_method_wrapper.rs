c4145c67686542ddf06b66b2c332844579118871
João Marcos Bezerra <joao.marcos@cloudwalk.io>
2024-10-18 22:12:54 -0300

    fix conditional compilation

--- src/eth/rpc/rpc_method_wrapper.rs
+++ src/eth/rpc/rpc_method_wrapper.rs
@@ -1,5 +1,6 @@
 use std::sync::Arc;
 
+use cfg_if::cfg_if;
 use jsonrpsee::types::Params;
 use jsonrpsee::Extensions;
 
@@ -8,15 +9,28 @@ use super::RpcContext;
 use crate::eth::primitives::StratusError;
 use crate::infra::metrics::inc_executor_transaction_error_types;
 
-pub fn call_error_metrics_wrapper<F, T>(function: F) -> impl Fn(Params<'_>, Arc<RpcContext>, Extensions) -> Result<T, StratusError> + Clone
-where
-    F: Fn(Params<'_>, Arc<RpcContext>, &Extensions) -> Result<T, StratusError> + Clone,
-{
-    move |params, ctx, extensions| {
-        function(params, ctx, &extensions).inspect_err(|e| {
-            let error_type = <&'static str>::from(e);
-            let client = extensions.rpc_client();
-            inc_executor_transaction_error_types(error_type, client);
-        })
+cfg_if! {
+    if #[cfg(feature = "metrics")] {
+        pub fn call_error_metrics_wrapper<F, T>(function: F) -> impl Fn(Params<'_>, Arc<RpcContext>, Extensions) -> Result<T, StratusError> + Clone
+        where
+            F: Fn(Params<'_>, Arc<RpcContext>, &Extensions) -> Result<T, StratusError> + Clone,
+        {
+            move |params, ctx, extensions| {
+                function(params, ctx, &extensions).inspect_err(|e| {
+                    let error_type = <&'static str>::from(e);
+                    let client = extensions.rpc_client();
+                    inc_executor_transaction_error_types(error_type, client);
+                })
+            }
+        }
+    } else {
+        pub fn call_error_metrics_wrapper<F, T>(function: F) -> impl Fn(Params<'_>, Arc<RpcContext>, Extensions) -> Result<T, StratusError> + Clone
+        where
+            F: Fn(Params<'_>, Arc<RpcContext>, &Extensions) -> Result<T, StratusError> + Clone,
+        {
+            move |params, ctx, extensions| {
+                function(params, ctx, &extensions)
+            }
+        }
     }
 }
