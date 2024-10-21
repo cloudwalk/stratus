eb3b15854f8a27689ae2c5c1114bf25be0ab2890
gabriel-aranha-cw <gabriel.aranha@cloudwalk.io>
2024-10-21 16:35:34 -0300

    fix: codacy issue

--- utils/deploy/deploy.py
+++ utils/deploy/deploy.py
@@ -194,116 +194,113 @@ def main() -> None:
     args = parser.parse_args()
 
     try:
-        log_file = open(args.log_file, "a")
+        with open(args.log_file, "a") as log_file:
+            try:
+                config = Config(
+                    leader_address=f"{args.current_leader}/?app=deploy",
+                    follower_address=f"{args.current_follower}/?app=deploy",
+                    auto_approve=args.auto_approve
+                )
+            except AttributeError as e:
+                log(message=f"Error initializing Config: {str(e)}", error=True)
+                raise DeploymentError("Error initializing Config", error_type="InitializationError")
+
+            if config.leader_address == config.follower_address:
+                log(message="Error: Leader and follower addresses must be different.")
+                raise DeploymentError("Leader and follower addresses must be different", error_type="InputValidationError")
+
+            start_time = time.time()
+
+            try:
+                # Validate initial health and state
+                validate_health(address=config.leader_address, role=NodeRole.LEADER)
+                validate_health(address=config.follower_address, role=NodeRole.FOLLOWER)
+                validate_state(address=config.leader_address, expected_state=True, role=NodeRole.LEADER)
+                validate_state(address=config.follower_address, expected_state=False, role=NodeRole.FOLLOWER)
+
+                if not config.auto_approve:
+                    confirmation = input("Do you want to proceed with pausing transactions? (yes/no): ")
+                    if confirmation.lower() != "yes":
+                        print("Script canceled.")
+                        return
+
+                # Disable transactions
+                toggle_transactions(address=config.leader_address, enable=False, role=NodeRole.LEADER)
+                toggle_transactions(address=config.follower_address, enable=False, role=NodeRole.FOLLOWER)
+
+                # Check pending transactions
+                if not has_pending_transactions(address=config.leader_address, total_attempts=config.total_check_attempts, required_checks=config.required_consecutive_checks, sleep_interval=config.sleep_interval):
+                    toggle_transactions(address=config.leader_address, enable=True, role=NodeRole.LEADER)
+                    toggle_transactions(address=config.follower_address, enable=True, role=NodeRole.FOLLOWER)
+                    return
+
+                if not config.auto_approve:
+                    confirmation = input("Do you want to proceed with pausing the miner? (yes/no): ")
+                    if confirmation.lower() != "yes":
+                        print("Script canceled.")
+                        # Toggle transactions back on
+                        toggle_transactions(address=config.leader_address, enable=True, role=NodeRole.LEADER)
+                        toggle_transactions(address=config.follower_address, enable=True, role=NodeRole.FOLLOWER)
+                        return
+
+                # Pause miner on Leader
+                toggle_miner(address=config.leader_address, enable=False)
+
+                # Check block synchronization
+                if not blocks_synced(leader_address=config.leader_address, follower_address=config.follower_address, total_attempts=config.total_check_attempts, required_checks=config.required_consecutive_checks, sleep_interval=config.sleep_interval):
+                    toggle_miner(address=config.leader_address, enable=True)
+                    toggle_transactions(address=config.leader_address, enable=True, role=NodeRole.LEADER)
+                    toggle_transactions(address=config.follower_address, enable=True, role=NodeRole.FOLLOWER)
+                    return
+
+                if not config.auto_approve:
+                    confirmation = input("Do you want to proceed with changing the Leader to Follower? (yes/no): ")
+                    if confirmation.lower() != "yes":
+                        print("Script canceled.")
+                        # Toggle miner back on
+                        toggle_miner(address=config.leader_address, enable=True)
+                        # Toggle transactions back on
+                        toggle_transactions(address=config.leader_address, enable=True, role=NodeRole.LEADER)
+                        toggle_transactions(address=config.follower_address, enable=True, role=NodeRole.FOLLOWER)
+                        return
+
+                # Change Leader to Follower
+                change_to_follower_params = [f"http://{config.follower_address}/", f"ws://{config.follower_address}/", config.external_rpc_timeout, config.sync_interval]
+                change_role(address=config.leader_address, method="stratus_changeToFollower", params=change_to_follower_params, role=NodeRole.FOLLOWER)
+
+                # Validate new Follower state
+                validate_state(address=config.leader_address, expected_state=False, role=NodeRole.FOLLOWER)
+
+                # Check if any leader or follower are leaders
+                if has_leader(config.leader_address, config.follower_address):
+                    log(message="Error: One of the nodes is currently a leader. Aborting leader switch.")
+                    return
+
+                if not config.auto_approve:
+                    confirmation = input("Do you want to proceed changing the Follower to Leader? (yes/no): ")
+                    if confirmation.lower() != "yes":
+                        print("Script canceled.")
+                        return
+
+                # Change Follower to Leader
+                change_role(address=config.follower_address, method="stratus_changeToLeader", params=[], role=NodeRole.LEADER)
+
+                # Validate new Leader state
+                validate_state(address=config.follower_address, expected_state=True, role=NodeRole.LEADER)
+
+                # Enable transactions on new Leader and Follower
+                toggle_transactions(address=config.follower_address, enable=True, role=NodeRole.LEADER)
+                toggle_transactions(address=config.leader_address, enable=True, role=NodeRole.FOLLOWER)
+
+                end_time = time.time()
+                elapsed_time = end_time - start_time
+                log(message=f"Process completed in {elapsed_time:.2f} seconds.")
+            except DeploymentError as e:
+                log(message=f"Process failed: {str(e)}", error=True)
+
     except IOError as e:
         log(message=f"Error opening log file: {e}")
         raise DeploymentError("Error initializing Log", error_type="InitializationError")
-
-    try:
-        config = Config(
-            leader_address=f"{args.current_leader}/?app=deploy",
-            follower_address=f"{args.current_follower}/?app=deploy",
-            auto_approve=args.auto_approve
-        )
-    except AttributeError as e:
-        log(message=f"Error initializing Config: {str(e)}", error=True)
-        raise DeploymentError("Error initializing Config", error_type="InitializationError")
-
-    if config.leader_address == config.follower_address:
-        log(message="Error: Leader and follower addresses must be different.")
-        raise DeploymentError("Leader and follower addresses must be different", error_type="InputValidationError")
-
-    start_time = time.time()
-
-    try:
-        # Validate initial health and state
-        validate_health(address=config.leader_address, role=NodeRole.LEADER)
-        validate_health(address=config.follower_address, role=NodeRole.FOLLOWER)
-        validate_state(address=config.leader_address, expected_state=True, role=NodeRole.LEADER)
-        validate_state(address=config.follower_address, expected_state=False, role=NodeRole.FOLLOWER)
-
-        if not config.auto_approve:
-            confirmation = input("Do you want to proceed with pausing transactions? (yes/no): ")
-            if confirmation.lower() != "yes":
-                print("Script canceled.")
-                return
-
-        # Disable transactions
-        toggle_transactions(address=config.leader_address, enable=False, role=NodeRole.LEADER)
-        toggle_transactions(address=config.follower_address, enable=False, role=NodeRole.FOLLOWER)
-
-        # Check pending transactions
-        if not has_pending_transactions(address=config.leader_address, total_attempts=config.total_check_attempts, required_checks=config.required_consecutive_checks, sleep_interval=config.sleep_interval):
-            toggle_transactions(address=config.leader_address, enable=True, role=NodeRole.LEADER)
-            toggle_transactions(address=config.follower_address, enable=True, role=NodeRole.FOLLOWER)
-            return
-
-        if not config.auto_approve:
-            confirmation = input("Do you want to proceed with pausing the miner? (yes/no): ")
-            if confirmation.lower() != "yes":
-                print("Script canceled.")
-                # Toggle transactions back on
-                toggle_transactions(address=config.leader_address, enable=True, role=NodeRole.LEADER)
-                toggle_transactions(address=config.follower_address, enable=True, role=NodeRole.FOLLOWER)
-                return
-
-        # Pause miner on Leader
-        toggle_miner(address=config.leader_address, enable=False)
-
-        # Check block synchronization
-        if not blocks_synced(leader_address=config.leader_address, follower_address=config.follower_address, total_attempts=config.total_check_attempts, required_checks=config.required_consecutive_checks, sleep_interval=config.sleep_interval):
-            toggle_miner(address=config.leader_address, enable=True)
-            toggle_transactions(address=config.leader_address, enable=True, role=NodeRole.LEADER)
-            toggle_transactions(address=config.follower_address, enable=True, role=NodeRole.FOLLOWER)
-            return
-
-        if not config.auto_approve:
-            confirmation = input("Do you want to proceed with changing the Leader to Follower? (yes/no): ")
-            if confirmation.lower() != "yes":
-                print("Script canceled.")
-                # Toggle miner back on
-                toggle_miner(address=config.leader_address, enable=True)
-                # Toggle transactions back on
-                toggle_transactions(address=config.leader_address, enable=True, role=NodeRole.LEADER)
-                toggle_transactions(address=config.follower_address, enable=True, role=NodeRole.FOLLOWER)
-                return
-
-        # Change Leader to Follower
-        change_to_follower_params = [f"http://{config.follower_address}/", f"ws://{config.follower_address}/", config.external_rpc_timeout, config.sync_interval]
-        change_role(address=config.leader_address, method="stratus_changeToFollower", params=change_to_follower_params, role=NodeRole.FOLLOWER)
-
-        # Validate new Follower state
-        validate_state(address=config.leader_address, expected_state=False, role=NodeRole.FOLLOWER)
-
-        # Check if any leader or follower are leaders
-        if has_leader(config.leader_address, config.follower_address):
-            log(message="Error: One of the nodes is currently a leader. Aborting leader switch.")
-            return
-
-        if not config.auto_approve:
-            confirmation = input("Do you want to proceed changing the Follower to Leader? (yes/no): ")
-            if confirmation.lower() != "yes":
-                print("Script canceled.")
-                return
-
-        # Change Follower to Leader
-        change_role(address=config.follower_address, method="stratus_changeToLeader", params=[], role=NodeRole.LEADER)
-
-        # Validate new Leader state
-        validate_state(address=config.follower_address, expected_state=True, role=NodeRole.LEADER)
-
-        # Enable transactions on new Leader and Follower
-        toggle_transactions(address=config.follower_address, enable=True, role=NodeRole.LEADER)
-        toggle_transactions(address=config.leader_address, enable=True, role=NodeRole.FOLLOWER)
-
-        end_time = time.time()
-        elapsed_time = end_time - start_time
-        log(message=f"Process completed in {elapsed_time:.2f} seconds.")
-    except DeploymentError as e:
-        log(message=f"Process failed: {str(e)}", error=True)
-    finally:
-        if log_file:
-            log_file.close()
 
 if __name__ == "__main__":
     main()
