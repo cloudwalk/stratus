49fab7b820177ccec365b5e93f4ebe66472e724d
gabriel-aranha-cw <gabriel.aranha@cloudwalk.io>
2024-10-24 15:52:01 -0300

    feat: add initial e2e boilerplate

---
+++ e2e/cloudwalk-contracts/integration/test/leader-follower-kafka.test.ts
@@ -0,0 +1,51 @@
+import { expect } from "chai";
+
+import { consumeMessages, disconnectKafkaConsumer, initializeKafkaConsumer, subscribeToTopic } from "./helpers/kafka";
+import { sendWithRetry, updateProviderUrl } from "./helpers/rpc";
+
+describe("Leader & Follower Kafka integration test", function () {
+    it("Validate initial Leader state and health", async function () {
+        updateProviderUrl("stratus");
+        const leaderNode = await sendWithRetry("stratus_state", []);
+        expect(leaderNode.is_importer_shutdown).to.equal(true);
+        expect(leaderNode.is_interval_miner_running).to.equal(true);
+        expect(leaderNode.is_leader).to.equal(true);
+        expect(leaderNode.miner_paused).to.equal(false);
+        expect(leaderNode.transactions_enabled).to.equal(true);
+        const leaderHealth = await sendWithRetry("stratus_health", []);
+        expect(leaderHealth).to.equal(true);
+    });
+
+    it("Validate initial Follower state and health", async function () {
+        updateProviderUrl("stratus-follower");
+        const followerNode = await sendWithRetry("stratus_state", []);
+        expect(followerNode.is_importer_shutdown).to.equal(false);
+        expect(followerNode.is_interval_miner_running).to.equal(false);
+        expect(followerNode.is_leader).to.equal(false);
+        expect(followerNode.miner_paused).to.equal(false);
+        expect(followerNode.transactions_enabled).to.equal(true);
+        const followerHealth = await sendWithRetry("stratus_health", []);
+        expect(followerHealth).to.equal(true);
+    });
+
+    describe("Leader & Follower Kafka integration test", function () {
+        before(async function () {
+            await initializeKafkaConsumer("stratus-consumer");
+            await subscribeToTopic("stratus-events");
+        });
+
+        after(async function () {
+            await disconnectKafkaConsumer();
+        });
+
+        it("Consume events from Kafka topic", async function () {
+            const messages = await consumeMessages();
+            expect(messages.length).to.be.greaterThan(0);
+            
+            console.log("Consumed messages:");
+            messages.forEach((message, index) => {
+                console.log(`Message ${index + 1}:`, JSON.stringify(message, null, 2));
+            });
+        });
+    });
+});
--- justfile
+++ justfile
@@ -289,8 +289,16 @@ e2e-leader-follower-up test="brlc":
     just _wait_for_stratus 3000
 
     # Start Stratus with follower flag
-    RUST_BACKTRACE=1 RUST_LOG=info cargo run --release --bin stratus --features dev -- --follower --perm-storage=rocks --rocks-path-prefix=temp_3001 --tokio-console-address=0.0.0.0:6669 --metrics-exporter-address=0.0.0.0:9001 -a 0.0.0.0:3001 -r http://0.0.0.0:3000/ -w ws://0.0.0.0:3000/  --kafka-bootstrap-servers localhost:29092 --kafka-topic stratus-events --kafka-client-id stratus-producer --kafka-group-id stratus-consumer > e2e_logs/importer.log &
-
+    if [ "{{test}}" = "kafka" ]; then
+    # Start Kafka using Docker Compose
+        just _log "Starting Kafka"
+        docker-compose up kafka >> e2e_logs/kafka.log &
+        just _log "Waiting Kafka start"
+        sleep 40
+        RUST_BACKTRACE=1 RUST_LOG=info cargo run --release --bin stratus --features dev -- --follower --perm-storage=rocks --rocks-path-prefix=temp_3001 --tokio-console-address=0.0.0.0:6669 --metrics-exporter-address=0.0.0.0:9001 -a 0.0.0.0:3001 -r http://0.0.0.0:3000/ -w ws://0.0.0.0:3000/ --kafka-bootstrap-servers localhost:29092 --kafka-topic stratus-events --kafka-client-id stratus-producer --kafka-group-id stratus-consumer > e2e_logs/importer.log &
+    else
+        RUST_BACKTRACE=1 RUST_LOG=info cargo run --release --bin stratus --features dev -- --follower --perm-storage=rocks --rocks-path-prefix=temp_3001 --tokio-console-address=0.0.0.0:6669 --metrics-exporter-address=0.0.0.0:9001 -a 0.0.0.0:3001 -r http://0.0.0.0:3000/ -w ws://0.0.0.0:3000/ > e2e_logs/importer.log &
+    fi
     # Wait for Stratus with follower flag to start
     just _wait_for_stratus 3001
 
@@ -344,6 +352,9 @@ e2e-leader-follower-down:
     killport 3000
     stratus_pid=$(pgrep -f 'stratus')
     kill $stratus_pid
+
+    # Kill Kafka
+    docker-compose down
 
     # Delete data contents
     rm -rf ./temp_*
