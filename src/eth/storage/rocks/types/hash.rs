61c1cb7d80b10f2e2126c457de7d0baf35bc629f
Daniel Freire <daniel.freire@cloudwalk.io>
2024-10-30 16:44:48 -0300

    ling

--- src/eth/storage/rocks/types/hash.rs
+++ src/eth/storage/rocks/types/hash.rs
@@ -1,9 +1,9 @@
 use std::fmt::Debug;
+
 use crate::eth::primitives::Hash;
 
 #[derive(Debug, Clone, Copy, Default, PartialEq, Eq, Hash, serde::Serialize, serde::Deserialize, fake::Dummy)]
 pub struct HashRocksdb([u8; 32]);
-
 
 impl From<Hash> for HashRocksdb {
     fn from(item: Hash) -> Self {
