f98c99d7c0ab394e593b336921fc80b9a6cd22c5
João Marcos <164224824+marcospb19-cw@users.noreply.github.com>
2024-11-06 13:48:27 -0300

    chore: update default env var values (#1851)
    vars updated: ASYNC_THREADS, BLOCKING_THREADS, EXECUTOR_EVMS,
    MAX_CONNECTIONS, MAX_SUBSCRIPTIONS, TEMP_STORAGE

--- src/eth/rpc/rpc_config.rs
+++ src/eth/rpc/rpc_config.rs
@@ -10,10 +10,10 @@ pub struct RpcServerConfig {
     pub rpc_address: SocketAddr,
 
     /// JSON-RPC server max active connections
-    #[arg(long = "max-connections", env = "MAX_CONNECTIONS", default_value = "200")]
+    #[arg(long = "max-connections", env = "MAX_CONNECTIONS", default_value = "400")]
     pub rpc_max_connections: u32,
 
     /// JSON-RPC server max active subscriptions per client.
-    #[arg(long = "max-subscriptions", env = "MAX_SUBSCRIPTIONS", default_value = "15")]
+    #[arg(long = "max-subscriptions", env = "MAX_SUBSCRIPTIONS", default_value = "30")]
     pub rpc_max_subscriptions: u32,
 }
