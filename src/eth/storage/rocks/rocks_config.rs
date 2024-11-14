981355c7013adf812ea214fb93145e4289918a53
Daniel Freire <daniel.freire@cloudwalk.io>
2024-11-14 10:55:25 -0300

    mmap

--- src/eth/storage/rocks/rocks_config.rs
+++ src/eth/storage/rocks/rocks_config.rs
@@ -28,6 +28,7 @@ impl DbConfig {
         opts.create_if_missing(true);
         opts.create_missing_column_families(true);
         opts.increase_parallelism(16);
+        opts.set_allow_mmap_reads(true);
 
         // NOTE: As per the rocks db wiki: "The overhead of statistics is usually small but non-negligible. We usually observe an overhead of 5%-10%."
         #[cfg(feature = "metrics")]
@@ -39,12 +40,10 @@ impl DbConfig {
         match self {
             DbConfig::OptimizedPointLookUp => {
                 let cuckoo = CuckooTableOptions::default();
-                opts.set_allow_mmap_reads(true);
                 opts.set_compression_type(rocksdb::DBCompressionType::None);
                 opts.set_cuckoo_table_factory(&cuckoo);
             }
             DbConfig::Default => {
-
                 let mut block_based_options = BlockBasedOptions::default();
 
                 block_based_options.set_pin_l0_filter_and_index_blocks_in_cache(true);
