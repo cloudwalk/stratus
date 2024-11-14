4a82465828f5ebf3ec1799c5d83db318541f2479
Daniel Freire <daniel.freire@cloudwalk.io>
2024-11-14 10:31:34 -0300

    test cuckoo table format

--- src/eth/storage/rocks/rocks_config.rs
+++ src/eth/storage/rocks/rocks_config.rs
@@ -1,5 +1,6 @@
 use rocksdb::BlockBasedOptions;
 use rocksdb::Cache;
+use rocksdb::CuckooTableOptions;
 use rocksdb::Options;
 
 pub enum CacheSetting {
@@ -21,16 +22,12 @@ impl Default for DbConfig {
 }
 
 impl DbConfig {
-    pub fn to_options(self, cache_setting: CacheSetting) -> Options {
+    pub fn to_options(self, _cache_setting: CacheSetting) -> Options {
         let mut opts = Options::default();
-        let mut block_based_options = BlockBasedOptions::default();
 
         opts.create_if_missing(true);
         opts.create_missing_column_families(true);
         opts.increase_parallelism(16);
-        block_based_options.set_pin_l0_filter_and_index_blocks_in_cache(true);
-        block_based_options.set_cache_index_and_filter_blocks(true);
-        block_based_options.set_ribbon_filter(15.5);
 
         // NOTE: As per the rocks db wiki: "The overhead of statistics is usually small but non-negligible. We usually observe an overhead of 5%-10%."
         #[cfg(feature = "metrics")]
@@ -41,22 +38,27 @@ impl DbConfig {
 
         match self {
             DbConfig::OptimizedPointLookUp => {
+                let mut cuckoo = CuckooTableOptions::default();
+                cuckoo.set_hash_ratio(0.5);
+                cuckoo.set_max_search_depth(200);
                 opts.set_compression_type(rocksdb::DBCompressionType::None);
+                opts.set_cuckoo_table_factory(&cuckoo);
             }
             DbConfig::Default => {
+
+                let mut block_based_options = BlockBasedOptions::default();
+
+                block_based_options.set_pin_l0_filter_and_index_blocks_in_cache(true);
+                block_based_options.set_cache_index_and_filter_blocks(true);
+                block_based_options.set_ribbon_filter(15.5);
+
                 opts.set_compression_type(rocksdb::DBCompressionType::Lz4);
                 opts.set_bottommost_compression_type(rocksdb::DBCompressionType::Zstd);
-                opts.set_bottommost_compression_options(-14, 32767, 0, 16 * 1024, true); // mostry defaults except max_dict_bytes
+                opts.set_bottommost_compression_options(-14, 32767, 0, 16 * 1024, true); // mostly defaults except max_dict_bytes
                 opts.set_bottommost_zstd_max_train_bytes(1600 * 1024, true);
             }
         }
 
-        if let CacheSetting::Enabled(cache_size) = cache_setting {
-            let cache = Cache::new_lru_cache(cache_size);
-            block_based_options.set_block_cache(&cache);
-        }
-
-        opts.set_block_based_table_factory(&block_based_options);
         opts
     }
 }
