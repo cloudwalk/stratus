a500ab0277f12371041bbbd151aa8d5bcb148594
João Marcos Bezerra <joao.marcos@cloudwalk.io>
2024-12-05 22:57:54 -0300

    enha: tidying rocksdb - log index and tx index as u32

--- src/eth/storage/permanent/rocks/types/index.rs
+++ src/eth/storage/permanent/rocks/types/index.rs
@@ -3,22 +3,16 @@ use std::fmt::Debug;
 use crate::eth::primitives::Index;
 
 #[derive(Debug, Clone, PartialEq, Eq, serde::Serialize, serde::Deserialize, derive_more::Add, Copy, Hash, fake::Dummy)]
-pub struct IndexRocksdb(u64);
-
-impl IndexRocksdb {
-    pub fn inner_value(&self) -> u64 {
-        self.0
-    }
-}
+pub struct IndexRocksdb(pub(self) u32);
 
 impl From<Index> for IndexRocksdb {
     fn from(item: Index) -> Self {
-        IndexRocksdb(item.0)
+        IndexRocksdb(item.0 as u32)
     }
 }
 
 impl From<IndexRocksdb> for Index {
     fn from(item: IndexRocksdb) -> Self {
-        Index::new(item.inner_value())
+        Index::new(item.0 as u64)
     }
 }
