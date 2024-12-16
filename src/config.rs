e8b3f862a7f1eaf487e1bf697d476bcb959a874a
gabriel-aranha-cw <gabriel.aranha@cloudwalk.io>
2024-12-16 18:50:32 -0300

    fix: lint

--- src/config.rs
+++ src/config.rs
@@ -150,7 +150,7 @@ impl CommonConfig {
                 let async_id = ASYNC_ID.fetch_add(1, Ordering::SeqCst);
                 if async_id <= num_async_threads {
                     if cfg!(feature = "flamegraph") {
-                        return "tokio-async".to_string()
+                        return "tokio-async".to_string();
                     } else {
                         return format!("tokio-async-{}", async_id);
                     }
