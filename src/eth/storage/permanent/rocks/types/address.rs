cf01ce7628e5b66333f3849272aaf9a9110fda12
carneiro-cw <156914855+carneiro-cw@users.noreply.github.com>
2025-01-16 12:32:18 -0300

    chore: remove more unused code (#1954)

--- src/eth/storage/permanent/rocks/types/address.rs
+++ src/eth/storage/permanent/rocks/types/address.rs
@@ -5,12 +5,6 @@ use crate::eth::primitives::Address;
 #[derive(Debug, Clone, Copy, Default, PartialEq, Eq, Hash, serde::Serialize, serde::Deserialize, fake::Dummy)]
 pub struct AddressRocksdb([u8; 20]);
 
-impl AddressRocksdb {
-    pub fn inner_value(&self) -> [u8; 20] {
-        self.0
-    }
-}
-
 impl From<Address> for AddressRocksdb {
     fn from(item: Address) -> Self {
         AddressRocksdb(item.0.into())
