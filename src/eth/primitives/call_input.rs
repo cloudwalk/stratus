1f547898774348ceb54bbc22cb2f83c05930b200
Daniel Freire <daniel.freire@cloudwalk.io>
2025-01-29 04:50:09 -0300

    enha: handle duplicate input/data when deserializing the CallInput

--- src/eth/primitives/call_input.rs
+++ src/eth/primitives/call_input.rs
@@ -1,8 +1,20 @@
 use display_json::DebugAsJson;
+use serde::Deserialize;
 
 use crate::eth::primitives::Address;
 use crate::eth::primitives::Bytes;
 use crate::eth::primitives::Wei;
+
+#[derive(serde::Deserialize)]
+struct CallInputDataField {
+    data: Option<Bytes>,
+    input: Option<Bytes>,
+}
+
+fn deserialize_data_field<'d, D: serde::Deserializer<'d>>(d: D) -> Result<Bytes, D::Error> {
+    let CallInputDataField { data, input } = CallInputDataField::deserialize(d)?;
+    Ok(data.or(input).unwrap_or_default())
+}
 
 #[derive(DebugAsJson, Clone, PartialEq, Eq, fake::Dummy, serde::Serialize, serde::Deserialize)]
 pub struct CallInput {
@@ -15,6 +27,6 @@ pub struct CallInput {
     #[serde(rename = "value", default)]
     pub value: Wei,
 
-    #[serde(rename = "data", alias = "input", default)]
+    #[serde(deserialize_with = "deserialize_data_field", flatten, default)]
     pub data: Bytes,
 }
