b7019d6eb2a7c13b90a23075e6d3c6931bd02102
gabriel-aranha-cw <gabriel.aranha@cloudwalk.io>
2025-01-30 09:39:25 -0300

    chore: move line

--- src/alias.rs
+++ src/alias.rs
@@ -15,7 +15,6 @@ pub type JsonValue = serde_json::Value;
 pub type AlloyBlockVoid = alloy_rpc_types_eth::Block<()>;
 pub type AlloyBlockAlloyTransaction = alloy_rpc_types_eth::Block<alloy_rpc_types_eth::Transaction>;
 pub type AlloyBlockAlloyExternalTransaction = alloy_rpc_types_eth::Block<ExternalTransaction>;
-pub type AlloyBlockEthersTransaction = alloy_rpc_types_eth::Block<ethers_core::types::Transaction>;
 pub type AlloyBlockH256 = alloy_rpc_types_eth::Block<H256>;
 pub type AlloyBytes = alloy_primitives::Bytes;
 pub type AlloyLog = alloy_rpc_types_eth::Log;
@@ -34,6 +33,7 @@ pub type AlloyUint256 = alloy_primitives::Uint<256, 4>;
 // -----------------------------------------------------------------------------
 // Ethers
 // -----------------------------------------------------------------------------
+pub type AlloyBlockEthersTransaction = alloy_rpc_types_eth::Block<ethers_core::types::Transaction>;
 pub type EthersBlockExternalTransaction = ethers_core::types::Block<ExternalTransaction>;
 pub type EthersBytes = ethers_core::types::Bytes;
 pub type EthersTransaction = ethers_core::types::Transaction;
