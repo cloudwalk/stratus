2e7a831d96a1543597e797d9a531c9f29e9c72c3
gabriel-aranha-cw <gabriel.aranha@cloudwalk.io>
2025-03-14 13:50:08 -0300

    chore: add TODOs for naming improvements and newtypes in BlockChangesRocksdb
    - Added TODO comments to improve naming conventions.
    - Suggested creating newtypes for Address and ExecutionAccountChanges in the BlockChangesRocksdb struct.

--- src/eth/storage/permanent/rocks/types/changes.rs
+++ src/eth/storage/permanent/rocks/types/changes.rs
@@ -5,6 +5,8 @@ use crate::eth::primitives::Address;
 use crate::eth::primitives::ExecutionAccountChanges;
 
 #[derive(Debug, Clone, PartialEq, Serialize, Deserialize, fake::Dummy)]
+// TODO: improve naming
+// TODO: create rocksdb newtypes for Address and ExecutionAccountChanges
 pub struct BlockChangesRocksdb(pub Vec<(Address, ExecutionAccountChanges)>);
 
 impl From<Vec<(Address, ExecutionAccountChanges)>> for BlockChangesRocksdb {
