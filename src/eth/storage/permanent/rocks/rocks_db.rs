fb9ca8e0f28e7c486512a2ac33f59912e577b83b
gabriel-aranha-cw <gabriel.aranha@cloudwalk.io>
2025-03-19 13:34:00 -0300

    refactor: update create_or_open_db to accept Vec for column family options
    - Changed the parameter type of `create_or_open_db` from `&[(&str, Options)]` to `&Vec<(&str, Options)>` for consistency with previous refactoring efforts.

--- src/eth/storage/permanent/rocks/rocks_db.rs
+++ src/eth/storage/permanent/rocks/rocks_db.rs
@@ -17,7 +17,7 @@ use crate::infra::metrics;
 ///
 /// The returned `Options` **need** to be stored to refer to the DB metrics!
 #[tracing::instrument(skip_all, fields(path = ?path.as_ref()))]
-pub fn create_or_open_db(path: impl AsRef<Path>, cf_configs: &[(&str, Options)]) -> anyhow::Result<(Arc<DB>, Options)> {
+pub fn create_or_open_db(path: impl AsRef<Path>, cf_configs: &Vec<(&str, Options)>) -> anyhow::Result<(Arc<DB>, Options)> {
     let path = path.as_ref();
 
     tracing::debug!("creating settings for each column family");
