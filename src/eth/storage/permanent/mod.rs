a99b65a153ade4b6de99f0d3944f411e532053af
gabriel-aranha-cw <gabriel.aranha@cloudwalk.io>
2025-03-21 10:13:38 -0300

    feat: add save_genesis_block method to PermanentStorage trait
    - Introduced the `save_genesis_block` method to the `PermanentStorage` trait, allowing for the persistence of the genesis block and associated accounts in development mode. This addition enhances the functionality of the storage interface.

--- src/eth/storage/permanent/mod.rs
+++ src/eth/storage/permanent/mod.rs
@@ -71,6 +71,7 @@ pub trait PermanentStorage: Send + Sync + 'static {
     fn rocksdb_replication_enabled(&self) -> bool;
 
     #[cfg(feature = "dev")]
+    /// Persists the genesis block and accounts.
     fn save_genesis_block(&self, block: Block, accounts: Vec<Account>) -> anyhow::Result<(), StorageError>;
 
     // -------------------------------------------------------------------------
