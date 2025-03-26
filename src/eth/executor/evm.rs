3e366ff76971fd2e285685fc96f5dc8dd2371595
gabriel-aranha-cw <gabriel.aranha@cloudwalk.io>
2025-03-26 13:32:21 -0300

    test

--- src/eth/executor/evm.rs
+++ src/eth/executor/evm.rs
@@ -1,5 +1,5 @@
 use std::cmp::min;
-use std::collections::HashMap;
+use std::collections::BTreeMap;
 use std::sync::Arc;
 
 use alloy_consensus::transaction::TransactionInfo;
@@ -395,7 +395,7 @@ impl RevmSession {
             config,
             storage,
             input: EvmInput::default(),
-            storage_changes: HashMap::default(),
+            storage_changes: BTreeMap::default(),
             metrics: EvmExecutionMetrics::default(),
         }
     }
@@ -403,7 +403,7 @@ impl RevmSession {
     /// Resets the session to be used with a new transaction.
     pub fn reset(&mut self, input: EvmInput) {
         self.input = input;
-        self.storage_changes = HashMap::default();
+        self.storage_changes = BTreeMap::default();
         self.metrics = EvmExecutionMetrics::default();
     }
 }
