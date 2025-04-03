06bcd8e47679e0d9e1b9985b3c5e5532c23975ca
gabriel-aranha-cw <gabriel.aranha@cloudwalk.io>
2025-04-03 11:11:37 -0300

    debug

--- e2e/test/interval/evm-input.test.ts
+++ e2e/test/interval/evm-input.test.ts
@@ -11,14 +11,19 @@ describe("Evm Input", function () {
         await sendReset();
         const contract = await deployTestEvmInput();
         await contract.waitForDeployment();
+        console.log("Contract deployed");
+
         let tx = await contract.heavyComputation();
+        console.log("Heavy computation sent");
+
         let receipt = (await tx.wait()) as ContractTransactionReceipt;
-        const event = receipt.logs[0];
+        console.log("Heavy computation receipt");
+
+        const event = receipt.logs[0];        
         const logs = contract.interface.parseLog({
             topics: event.topics,
             data: event.data,
         })?.args as Result;
-
         expect(receipt.blockNumber).to.eq(logs.blockNumber);
         expect(logs.blockNumber).gte(1000);
         expect(logs.isSlow).to.be.false;
