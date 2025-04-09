a2988690ac57278df57eca1706c3cd972e051558
gabriel-aranha-cw <gabriel.aranha@cloudwalk.io>
2025-04-09 18:53:56 -0300

    debug

--- e2e/test/interval/evm-input.test.ts
+++ e2e/test/interval/evm-input.test.ts
@@ -6,12 +6,19 @@ import { deployTestEvmInput } from "../helpers/rpc";
 // This test needs to be ran with stratus on a very fast block production rate (around 10ms is fast enough)
 describe("Evm Input", function () {
     it("should not be executed in one block but added to another", async function () {
+        this.timeout(300000);
+
+        console.log("Starting test");
+
         const contract = await deployTestEvmInput();
         await contract.waitForDeployment();
+        console.log("Contract deployed");
 
         let tx = await contract.heavyComputation();
+        console.log("Heavy computation sent");
 
         let receipt = (await tx.wait()) as ContractTransactionReceipt;
+        console.log("Heavy computation receipt");
 
         const event = receipt.logs[0];
         const logs = contract.interface.parseLog({
