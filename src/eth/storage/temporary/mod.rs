3196e37bdba99ab8c5f144d64a256065f86dbd27
Daniel Freire <daniel.freire@cloudwalk.io>
2025-04-11 12:53:57 -0300

    fix: start temporary storage at block 0 if perm has no genesis

--- src/eth/storage/temporary/mod.rs
+++ src/eth/storage/temporary/mod.rs
@@ -5,6 +5,8 @@ mod inmemory;
 use clap::Parser;
 use display_json::DebugAsJson;
 
+use crate::eth::primitives::BlockNumber;
+
 use super::RocksPermanentStorage;
 
 // -----------------------------------------------------------------------------
@@ -21,7 +23,10 @@ impl TemporaryStorageConfig {
     /// Initializes temporary storage implementation.
     pub fn init(&self, perm_storage: &RocksPermanentStorage) -> anyhow::Result<InMemoryTemporaryStorage> {
         tracing::info!(config = ?self, "creating temporary storage");
-        let pending_block_number = perm_storage.read_mined_block_number()? + 1;
+        let pending_block_number = match perm_storage.has_genesis()? {
+            true => perm_storage.read_mined_block_number()? + 1,
+            false => BlockNumber::ZERO,
+        };
         Ok(InMemoryTemporaryStorage::new(pending_block_number))
     }
 }
