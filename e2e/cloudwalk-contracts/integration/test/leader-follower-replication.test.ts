f731ed25a159e13a11d36cf9159edf06c04432bb
gabriel-aranha-cw <gabriel.aranha@cloudwalk.io>
2025-05-15 14:48:03 -0300

    fix: replication

--- e2e/cloudwalk-contracts/integration/test/leader-follower-replication.test.ts
+++ e2e/cloudwalk-contracts/integration/test/leader-follower-replication.test.ts
@@ -82,6 +82,8 @@ describe("Leader & Follower replication integration test", function () {
 
                 const transactionInterval = 1000 / params.tps;
 
+                updateProviderUrl("stratus-follower");
+
                 let nonces = await Promise.all(
                     wallets.map((wallet) => sendWithRetry("eth_getTransactionCount", [wallet.address, "latest"])),
                 );
