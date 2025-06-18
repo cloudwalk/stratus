92408e59f99be1e880990b1e77473996d1dc85b8
Daniel Freire <daniel.freire@cloudwalk.io>
2025-06-18 10:03:16 -0300

    chore: change error msgs to make stratus compatible with callOptionalSignature (OpenZepellin)

--- src/eth/primitives/stratus_error.rs
+++ src/eth/primitives/stratus_error.rs
@@ -67,7 +67,7 @@ pub enum RpcError {
 #[derive(Debug, thiserror::Error, strum::EnumProperty, strum::IntoStaticStr, ErrorCode)]
 #[major_error_code = 2000]
 pub enum TransactionError {
-    #[error("Account at {address} is not a contract.")]
+    #[error("function selector was not recognized: Account at {address} is not a contract.")]
     #[error_code = 1]
     AccountNotContract { address: Address },
 
@@ -75,7 +75,7 @@ pub enum TransactionError {
     #[error_code = 2]
     Nonce { transaction: Nonce, account: Nonce },
 
-    #[error("Failed to execute transaction in EVM: {0:?}.")]
+    #[error("evm execution error: {0:?}.")]
     #[error_code = 3]
     EvmFailed(String), // TODO: split this in multiple errors
 
@@ -87,7 +87,7 @@ pub enum TransactionError {
     #[error_code = 5]
     ForwardToLeaderFailed,
 
-    #[error("Transaction reverted during execution.")]
+    #[error("Transaction reverted during execution. output: {output}")]
     #[error_code = 6]
     RevertedCall { output: Bytes },
 
@@ -95,7 +95,7 @@ pub enum TransactionError {
     #[error_code = 7]
     FromZeroAddress,
 
-    #[error("Transaction reverted during execution.")]
+    #[error("Transaction reverted during execution. reason: {reason}")]
     #[error_code = 8]
     RevertedCallWithReason { reason: RevertReason },
 }
