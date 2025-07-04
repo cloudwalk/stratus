3f54e94ccf4c072e4eeb1849ae5260f0c6367ad8
Daniel Freire <daniel.freire@cloudwalk.io>
2025-07-04 11:39:06 -0300

    chore: put replication behind dev FF

--- src/bin/historic_events_processor.rs
+++ src/bin/historic_events_processor.rs
@@ -90,7 +90,15 @@ fn process_block_events(block: BlockRocksdb) -> Vec<String> {
 /// Main function that processes blockchain data and generates events
 fn main() -> Result<(), anyhow::Error> {
     tracing_subscriber::fmt::init();
-    let state = RocksStorageState::new("data/rocksdb".to_string(), TIMEOUT, Some(0.1), false, false).context("failed to create rocksdb state")?;
+    let state = RocksStorageState::new(
+        "data/rocksdb".to_string(),
+        TIMEOUT,
+        Some(0.1),
+        false,
+        #[cfg(feature = "dev")]
+        false,
+    )
+    .context("failed to create rocksdb state")?;
 
     let (b_pb, tx_pb) = create_progress_bar(&state);
 
