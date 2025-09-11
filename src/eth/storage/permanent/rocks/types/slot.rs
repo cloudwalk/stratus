2c395ce7c2eff1255ea7d0dd66e77fa12a60a29f
Daniel Freire <daniel.freire@cloudwalk.io>
2025-09-11 00:40:52 -0300

    fix oopsie

--- src/eth/storage/permanent/rocks/types/slot.rs
+++ src/eth/storage/permanent/rocks/types/slot.rs
@@ -4,28 +4,8 @@ use crate::eth::primitives::SlotIndex;
 use crate::eth::primitives::SlotValue;
 use crate::eth::storage::permanent::rocks::SerializeDeserializeWithContext;
 
-#[derive(Clone, Debug, Copy, Default, PartialEq, Eq, bincode::Encode, bincode::Decode, fake::Dummy)]
+#[derive(Clone, Debug, Copy, Default, PartialEq, Eq, bincode::Encode, bincode::Decode, fake::Dummy, serde::Deserialize, serde::Serialize)]
 pub struct SlotValueRocksdb([u64; 4]);
-
-impl serde::Serialize for SlotValueRocksdb {
-    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
-    where
-        S: serde::Serializer,
-    {
-        let slot_value: SlotValue = (*self).into();
-        slot_value.serialize(serializer)
-    }
-}
-
-impl<'de> serde::Deserialize<'de> for SlotValueRocksdb {
-    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
-    where
-        D: serde::Deserializer<'de>,
-    {
-        let slot_value = SlotValue::deserialize(deserializer)?;
-        Ok(slot_value.into())
-    }
-}
 
 impl From<SlotValue> for SlotValueRocksdb {
     fn from(item: SlotValue) -> Self {
@@ -39,9 +19,7 @@ impl From<SlotValueRocksdb> for SlotValue {
     }
 }
 
-#[derive(
-    Clone, Debug, Copy, Default, Hash, PartialEq, Eq, PartialOrd, Ord, bincode::Encode, bincode::Decode, fake::Dummy, serde::Serialize, serde::Deserialize,
-)]
+#[derive(Clone, Debug, Copy, Default, Hash, PartialEq, Eq, PartialOrd, Ord, bincode::Encode, bincode::Decode, fake::Dummy)]
 pub struct SlotIndexRocksdb([u64; 4]);
 
 impl From<SlotIndex> for SlotIndexRocksdb {
@@ -53,6 +31,26 @@ impl From<SlotIndex> for SlotIndexRocksdb {
 impl From<SlotIndexRocksdb> for SlotIndex {
     fn from(item: SlotIndexRocksdb) -> Self {
         SlotIndex::from(item.0)
+    }
+}
+
+impl serde::Serialize for SlotIndexRocksdb {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: serde::Serializer,
+    {
+        let slot_index: SlotIndex = (*self).into();
+        slot_index.serialize(serializer)
+    }
+}
+
+impl<'de> serde::Deserialize<'de> for SlotIndexRocksdb {
+    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+    where
+        D: serde::Deserializer<'de>,
+    {
+        let slot_index = SlotIndex::deserialize(deserializer)?;
+        Ok(slot_index.into())
     }
 }
 
