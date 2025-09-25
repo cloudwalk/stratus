ded0100bf8f1505169a76c6d87183aac1b424d5c
Daniel Freire <daniel.freire@cloudwalk.io>
2025-09-25 18:13:18 -0300

    read from account

--- src/eth/executor/executor.rs
+++ src/eth/executor/executor.rs
@@ -390,7 +390,8 @@ impl Executor {
                 let evm_execution = self.evms.execute(evm_input.clone(), EvmRoute::External);
 
                 if evm_execution.is_err() {
-                    tracing::error!(from=?evm_input.from, "failed to reexecute external transaction (first attempt)");
+                    let account = self.storage.read_account(evm_input.from, PointInTime::Pending, ReadKind::Transaction)?;
+                    tracing::error!(?account, from=?evm_input.from, "failed to reexecute external transaction (first attempt)");
 
                     let mut tx_input: TransactionInput = tx.try_into()?;
                     tx_input.v += U64::ONE;
@@ -406,9 +407,11 @@ impl Executor {
                         if let Ok(exec) = evm_execution {
                             exec
                         } else {
+                            let account = self.storage.read_account(evm_input.from, PointInTime::Pending, ReadKind::Transaction)?;
+
                             let json_tx = to_json_string(&tx);
                             let json_receipt = to_json_string(&receipt);
-                            tracing::error!(from=?evm_input.from, reason = ?e, %block_number, tx_hash = %tx.hash(), %json_tx, %json_receipt, ?evm_input, "failed to reexecute external transaction");
+                            tracing::error!(?account, from=?evm_input.from, reason = ?e, %block_number, tx_hash = %tx.hash(), %json_tx, %json_receipt, ?evm_input, "failed to reexecute external transaction");
                             return Err(e.into());
                         }
                     }
