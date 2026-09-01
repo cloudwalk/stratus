9bd902f4d37926ab1ef112e3d7a775a0b7c3c89d
Daniel Freire <daniel.freire@cloudwalk.io>
2026-09-01 10:40:21 -0300

    transient_state_lock

--- src/eth/storage/stratus_storage.rs
+++ src/eth/storage/stratus_storage.rs
@@ -820,6 +820,7 @@ impl StratusStorage {
     }
 
     pub fn load_access_list(&self, access_list: AccessListOutput) {
+        let _guard = self.transient_state_lock.read();
         let mut account_addresses = vec![];
         let mut slot_keys = vec![];
         for (address, slots) in access_list {
