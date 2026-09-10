54bc56fc223e6884bacea508eef4a252a5ed36e8
Daniel Freire <daniel.freire@cloudwalk.io>
2026-09-10 18:46:05 -0300

    transaction worker

--- crates/stratus_metrics/src/definitions.rs
+++ crates/stratus_metrics/src/definitions.rs
@@ -73,9 +73,6 @@ metrics! {
         "Time executing a local transaction."
         histogram_duration executor_local_transaction{success, contract, function},
 
-        "Number of transactions waiting to acquire the local transaction execution lock."
-        gauge executor_local_transaction_lock_waiting{},
-
         "Number of transactions waiting to acquire the local transaction warmup semaphore."
         gauge executor_local_transaction_semaphore_waiting{},
 
